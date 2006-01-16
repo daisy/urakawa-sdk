50342fc4f8541bd7978998ce86f54eb8d105bc32
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-01-16 03:57:57 +0000

    - Daniel WECK * Added MinMap (FreeMind format) for the Brainstorming session on Use-Cases, Functional Requirements, High-level perspective (Application, Toolkit, Utilities, etc.).

---
+++ trunk/java/sandbox/Brainstorming_MindMap_MultimediaAuthoringApplication.mm
@@ -0,0 +1,123 @@
+<map version="0.8.FA Alpha 5a">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1137155954157" ID="Freemind_Link_337595978" MODIFIED="1137167231384" STYLE="bubble" TEXT="Multimedia Authoring Application">
+<edge COLOR="#666666" STYLE="sharp_bezier" WIDTH="1"/>
+<font BOLD="true" NAME="Lucida Sans" SIZE="14"/>
+<icon BUILTIN="gohome"/>
+<node CREATED="1137155967604" HGAP="82" ID="_" MODIFIED="1137379427747" POSITION="right" STYLE="bubble" TEXT="Urakawa Toolkit" VSHIFT="24">
+<cloud COLOR="#99ff99"/>
+<icon BUILTIN="bookmark"/>
+<node CREATED="1137157850668" ID="Freemind_Link_1546819662" MODIFIED="1137158492711" TEXT="Core (Multimedia Object Model)">
+<node CREATED="1137157916468" ID="Freemind_Link_1909036948" MODIFIED="1137382956495" TEXT="Timing and Synchronization">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1137158099191" ID="Freemind_Link_1964406368" MODIFIED="1137158492718" TEXT="Navigation">
+<node CREATED="1137158113329" ID="Freemind_Link_1547905411" MODIFIED="1137164936891" TEXT="Time-based">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1137158122305" ID="Freemind_Link_284286478" MODIFIED="1137165395428" TEXT="Structure/Semantic/Logical-based">
+<arrowlink DESTINATION="Freemind_Link_374301882" ENDARROW="Default" ENDINCLINATION="120;0;" ID="Freemind_Arrow_Link_1225664071" STARTARROW="None" STARTINCLINATION="120;0;"/>
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1137163428202" ID="Freemind_Link_1013151540" MODIFIED="1137164603783" TEXT="Visitor Pattern">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1137158152247" ID="Freemind_Link_754976718" MODIFIED="1137163460281" TEXT="Selection">
+<node CREATED="1137164328816" ID="Freemind_Link_430724871" MODIFIED="1137164614669" TEXT="Tree Fragment">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1137164324307" ID="Freemind_Link_489780503" MODIFIED="1137164328186" TEXT="Contiguous"/>
+<node CREATED="1137164346453" ID="Freemind_Link_392661809" MODIFIED="1137164348830" TEXT="Node Slicing"/>
+</node>
+<node CREATED="1137164257527" ID="Freemind_Link_374301882" MODIFIED="1137165395430" TEXT="Semantic Annotations/Extensible Node Properties">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1137163402248" ID="Freemind_Link_1811393424" MODIFIED="1137164655727" TEXT="AssetManager">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1137157970651" ID="Freemind_Link_1417897195" MODIFIED="1137165077438" TEXT="Data Input/Output">
+<node CREATED="1137157975580" ID="Freemind_Link_1700859806" MODIFIED="1137158692148" TEXT="Import">
+<node CREATED="1137164622236" ID="Freemind_Link_809656713" MODIFIED="1137164631134" TEXT="Daisy 2.02">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+</node>
+<node CREATED="1137157982488" ID="Freemind_Link_419229723" MODIFIED="1137158492742" TEXT="Export">
+<node CREATED="1137164634382" ID="Freemind_Link_1974756986" MODIFIED="1137164642231" TEXT="Daisy 2.02">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1137165033430" ID="Freemind_Link_1379145143" MODIFIED="1137165040029" TEXT="Daisy 3">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node CREATED="1137165008648" ID="Freemind_Link_1478145256" MODIFIED="1137165030983" TEXT="SMIL1.0 - 2.1 - NG">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1137165112926" ID="Freemind_Link_1752587769" MODIFIED="1137165201620" TEXT="Open/Save">
+<icon BUILTIN="help"/>
+<node CREATED="1137165117604" ID="Freemind_Link_1706021344" MODIFIED="1137165135621" TEXT="Urakawa Project Format">
+<node CREATED="1137165137359" ID="Freemind_Link_885503827" MODIFIED="1137165180206" TEXT="Edition Session (State)"/>
+<node CREATED="1137165142997" ID="Freemind_Link_1646475351" MODIFIED="1137165155315" TEXT="Serialized Object Model"/>
+</node>
+</node>
+</node>
+<node CREATED="1137163800636" ID="Freemind_Link_1758821664" MODIFIED="1137164872660" TEXT="Command Pattern">
+<icon BUILTIN="help"/>
+<node CREATED="1137163817060" ID="Freemind_Link_940957546" MODIFIED="1137165442316" TEXT="Undo-Redo History"/>
+</node>
+<node CREATED="1137164472235" ID="Freemind_Link_734212118" MODIFIED="1137164532015" TEXT="Clipboard Manager">
+<icon BUILTIN="help"/>
+<node CREATED="1137164880099" ID="Freemind_Link_705431765" MODIFIED="1137164885457" TEXT="Cut, Copy, Paste"/>
+</node>
+<node CREATED="1137164694043" ID="Freemind_Link_1145768954" MODIFIED="1137164706262" TEXT="Markup (Text) Editor">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node CREATED="1137155999409" FOLDED="true" HGAP="78" ID="Freemind_Link_1636069981" MODIFIED="1137379451310" POSITION="left" STYLE="bubble" TEXT="Application &quot;Front-end&quot;" VSHIFT="-1">
+<cloud COLOR="#ffffcc"/>
+<icon BUILTIN="idea"/>
+<node CREATED="1137156871992" ID="Freemind_Link_1133975033" MODIFIED="1137158492772" TEXT="Graphical User Interface"/>
+<node CREATED="1137157084283" ID="Freemind_Link_351280635" MODIFIED="1137158492774" TEXT="Utilities"/>
+</node>
+<node CREATED="1137165548823" HGAP="48" ID="Freemind_Link_78847175" MODIFIED="1137379455877" POSITION="right" STYLE="bubble" TEXT="Usage Scenarios&#xa;(pseudo-code)" VSHIFT="169">
+<icon BUILTIN="attach"/>
+<node CREATED="1137165637196" ID="Freemind_Link_1616793732" MODIFIED="1137186936347" TEXT="ImportExport-OpenSave">
+<hook NAME="accessories/plugins/NodeNote.properties">
+<text>This example outlines the difference between Open/Save and Import/Export.&#xa;&quot;daisySample_TEST1.ncc&quot; and &quot;daisySample_TEST2.ncc&quot; should be absolutely identical since they are both obtained from the authoring core data model (serialized in &quot;urakawaProjectSample.ukw&quot;).&#xa;&#x192;</text>
+</hook>
+<node CREATED="1137186397351" ID="Freemind_Link_545731044" MODIFIED="1137187385985" TEXT="UrakawaDocument doc = UrakawaDocumentFactory.createDocumentInstance();">
+<icon BUILTIN="full-1"/>
+</node>
+<node CREATED="1137186461012" ID="Freemind_Link_1462856849" MODIFIED="1137187392167" TEXT="doc.import(&quot;./daisySample.ncc&quot;, TYPE_DAISY2);">
+<icon BUILTIN="full-2"/>
+</node>
+<node CREATED="1137186492723" ID="Freemind_Link_1926649157" MODIFIED="1137187395259" TEXT="doc.export(&quot;./daisySample_TEST1.ncc&quot;, TYPE_DAISY2);">
+<icon BUILTIN="full-3"/>
+</node>
+<node CREATED="1137186528102" ID="Freemind_Link_99846874" MODIFIED="1137187400103" TEXT="doc.save(&quot;./urakawaProjectSample.ukw&quot;);">
+<icon BUILTIN="full-4"/>
+</node>
+<node CREATED="1137186566319" ID="Freemind_Link_1943101789" MODIFIED="1137187403655" TEXT="doc.close();">
+<icon BUILTIN="full-5"/>
+</node>
+<node CREATED="1137186586484" ID="Freemind_Link_182413585" MODIFIED="1137187408688" TEXT="doc.open(&quot;./urakawaProjectSample.ukw&quot;);">
+<icon BUILTIN="full-6"/>
+</node>
+<node CREATED="1137186608416" ID="Freemind_Link_836219037" MODIFIED="1137187411284" TEXT="doc.export(&quot;./daisySample_TEST2.ncc&quot;, TYPE_DAISY2)">
+<icon BUILTIN="full-7"/>
+</node>
+<node CREATED="1137187137275" ID="Freemind_Link_1203686638" LINK="#Freemind_Link_681032266" MODIFIED="1137187210820" TEXT="ModifyExample">
+<arrowlink DESTINATION="Freemind_Link_681032266" ENDARROW="Default" ENDINCLINATION="250;0;" ID="Freemind_Arrow_Link_1550547317" STARTARROW="None" STARTINCLINATION="250;0;"/>
+<icon BUILTIN="wizard"/>
+</node>
+</node>
+<node CREATED="1137187180513" HGAP="24" ID="Freemind_Link_681032266" MODIFIED="1137187219627" TEXT="ModifyExample" VSHIFT="55">
+<node CREATED="1137187230330" ID="Freemind_Link_1971768448" MODIFIED="1137187574484" TEXT="doc.rebuildTableOfContents(2); //2 levels: e.g. Chapter+Sub-section" VSHIFT="32"/>
+</node>
+</node>
+<node CREATED="1137382687064" ID="Freemind_Link_180082134" MODIFIED="1137382846086" POSITION="left" TEXT="Use-Cases" VSHIFT="167">
+<node CREATED="1137382691796" ID="Freemind_Link_493754498" LINK="http://cvs.sourceforge.net/viewcvs.py/urakawa/urakawa/design/usecases/UseCase-BusyProducer.txt?view=markup" MODIFIED="1137382956497" TEXT="BusyProducer"/>
+</node>
+</node>
+</map>
