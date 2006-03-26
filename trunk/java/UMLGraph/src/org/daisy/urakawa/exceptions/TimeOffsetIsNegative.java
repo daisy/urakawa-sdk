0a91ff714334764a6a9202cc1a8fd3d4006869fa
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-03-26 14:32:57 +0000

    - Daniel WECK * Introduced a new Exception.

--- trunk/java/UMLGraph/src/org/daisy/urakawa/exceptions/TimeOffsetIsNegative.java
+++ trunk/java/UMLGraph/src/org/daisy/urakawa/exceptions/TimeOffsetIsNegative.java
@@ -1,7 +1,7 @@
 package org.daisy.urakawa.exceptions;
 
 /**
- * This exception should be thrown when trying to use a time offset that is not allowed to be nagative.
+ * This exception should be raised when trying to use a time offset that is not allowed to be nagative.
  */
 public class TimeOffsetIsNegative extends CheckedException {
 }
