2593f5aaca33f227c7d743fa58896febd09850ce
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-03-27 10:26:11 +0000

    - Daniel WECK * Added helper method to determine negative offset values (needed for raising the correspondign exceptions).

--- trunk/java/UMLGraph/src/org/daisy/urakawa/media/Time.java
+++ trunk/java/UMLGraph/src/org/daisy/urakawa/media/Time.java
@@ -5,4 +5,9 @@ package org.daisy.urakawa.media;
  * Can be a negative/0/positive offset relative to the local timebase in the current context.
  */
 public interface Time {
+    /**
+     * a helper method to help determine {@link org.daisy.urakawa.exceptions.TimeOffsetIsNegative} 
+     * @return true if the associated time value is a negative offset (<0 "less than zero")
+     */
+    public boolean isNegativeTimeOffset();
 }
