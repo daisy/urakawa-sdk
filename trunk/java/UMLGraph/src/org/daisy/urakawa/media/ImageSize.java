45b0fed84bead578e7792255e97d236c8563f77a
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-03-27 10:33:56 +0000

    - Daniel WECK * Changed name.

--- trunk/java/UMLGraph/src/org/daisy/urakawa/media/ImageSize.java
+++ trunk/java/UMLGraph/src/org/daisy/urakawa/media/ImageSize.java
@@ -3,7 +3,7 @@ package org.daisy.urakawa.media;
 /**
  * 2D surface
  */
-public interface Surface2D {
+public interface ImageSize {
     /**
      * @return the width of the 2d surface in pixels (cannot be negative, the result type should be "unsigned int")
      */
