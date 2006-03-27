a86409c97c6c47c3a300408115cc8aa04f53335e
Daniel Weck <625201+danielweck@users.noreply.github.com>
2006-03-27 10:33:56 +0000

    - Daniel WECK * Changed name.

--- java/UMLGraph/src/org/daisy/urakawa/media/VideoMedia.java
+++ java/UMLGraph/src/org/daisy/urakawa/media/VideoMedia.java
@@ -3,5 +3,5 @@ package org.daisy.urakawa.media;
 /**
  * {@link Media#getType()} should return MediaType.VIDEO
  */
-public interface VideoMedia extends ClippedMedia, Surface2D {
+public interface VideoMedia extends ClippedMedia, ImageSize {
 }
