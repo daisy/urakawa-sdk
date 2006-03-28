e1b866549e450a114507f83d1dd052c03da561e5
Daniel Weck <625201+danielweck@users.noreply.github.com>
2006-03-28 06:14:48 +0000

    - Daniel WECK * Made some comments a little more explicit.

--- java/UMLGraph/src/org/daisy/urakawa/media/Time.java
+++ java/UMLGraph/src/org/daisy/urakawa/media/Time.java
@@ -2,6 +2,8 @@ package org.daisy.urakawa.media;
 
 /**
  * Abstract Time offset (could be in milliseconds, SMPTE, etc.).
+ * This really is an interface "lollypop" that should be extended.
+ * Typically, methods like getTimeMilliseconds(), getTimeSMPTE(), etc. should be available to the end-user of the API.
  * Can be a negative/0/positive offset relative to the local timebase in the current context.
  */
 public interface Time {
