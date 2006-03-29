31eaaa3a5ba3ef481e019072b2bafe9c3a3249de
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-03-29 06:32:38 +0000

    - Daniel WECK * Updated comments to give examples of use for such factory, in particular to apply the 'Flyweight' pattern which aim is to allocate memory efficiently when dealing whith thousands of instances (e.g. Text in a Daisy book).

--- trunk/java/UMLGraph/src/org/daisy/urakawa/media/MediaFactory.java
+++ trunk/java/UMLGraph/src/org/daisy/urakawa/media/MediaFactory.java
@@ -4,6 +4,20 @@ package org.daisy.urakawa.media;
  * Abstract factory pattern: from the API user perspective:
  * do not use constructors, use a factory instead
  * (which will delegate to the real constructor of its choice).
+ *
+ *  A Factory offers much more flexibility than standard constructors.
+ * For example, optimized constructors can be used for instanciating many
+ * objects at once (e.g. parallel processing).
+ * 
+ * Another example is to have a memory-efficient object allocator for
+ * when instanciating many objects of the same type throught the course
+ * of the execution of the program, by always returning the same "Flyweight"
+ * instance of the object (e.g. Text media object is likely to created thousands of times in a Daisy book,
+ * for each small fragment of text).
+ * Implementation of the "Flyweight" pattern are quite common:
+ * Dom4J (Namespace object), Swing (TreeRenderer), etc.
+ * More info: 
+ * http://exciton.cs.rice.edu/javaresources/DesignPatterns/FlyweightPattern.htm 
  * 
  * This factory may be implemented as a singleton, but this is not a requirement.
  * The implementation can decide what pattern suits it best.
