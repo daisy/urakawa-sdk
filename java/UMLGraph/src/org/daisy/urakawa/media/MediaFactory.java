016b6b6e2d5fb50d4dbcc78bb7fc0fa727ba4d80
Daniel Weck <625201+danielweck@users.noreply.github.com>
2006-03-29 06:12:29 +0000

    - Daniel WECK * Updated comments and UML presentation to include annotated dependencies on the actual 'real' Media that are created by this factory.

--- java/UMLGraph/src/org/daisy/urakawa/media/MediaFactory.java
+++ java/UMLGraph/src/org/daisy/urakawa/media/MediaFactory.java
@@ -2,7 +2,16 @@ package org.daisy.urakawa.media;
 
 /**
  * Abstract factory pattern: from the API user perspective:
- * do not use constructors, use a factory instead (which will delegate to the real constructor of its choice).
+ * do not use constructors, use a factory instead
+ * (which will delegate to the real constructor of its choice).
+ * 
+ * This factory may be implemented as a singleton, but this is not a requirement.
+ * The implementation can decide what pattern suits it best.
+ * 
+ * @depend - Create 1 AudioMedia
+ * @depend - Create 1 VideoMedia
+ * @depend - Create 1 ImageMedia
+ * @depend - Create 1 TextMedia
  */
 public interface MediaFactory {
     /**
