836f03d8f46651bd9b6ea632eff493ef3ed5da8c
Daniel Weck <625201+danielweck@users.noreply.github.com>
2006-04-04 14:19:18 +0000

    - Daniel WECK * Follow-on of previous commit: delete un-used files.

--- java/UMLGraph/src/org/daisy/urakawa/exceptions/ChannelNameAlreadyExistException.java
+++
@@ -1,8 +0,0 @@
-package org.daisy.urakawa.exceptions;
-
-/**
- * This exception should be thrown when trying to add a Channel
- * which name is already used in the list of current channels.
- */
-public class ChannelNameAlreadyExistException extends CheckedException {
-}
--- java/UMLGraph/src/org/daisy/urakawa/exceptions/ChannelNameDoesNotExistException.java
+++
@@ -1,8 +0,0 @@
-package org.daisy.urakawa.exceptions;
-
-/**
- * This exception should be thrown when trying to remove a Channel
- * which name does not exist in the list of current channels.
- */
-public class ChannelNameDoesNotExistException extends CheckedException {
-}
