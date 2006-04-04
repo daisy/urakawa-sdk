5f7403273c3cfe137a97ab6ccc08ab77399a5b2f
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-04-04 14:19:18 +0000

    - Daniel WECK * Follow-on of previous commit: delete un-used files.

--- trunk/java/UMLGraph/src/org/daisy/urakawa/exceptions/MethodParameterIsValueOutOfBoundsException.java
+++
@@ -1,8 +0,0 @@
-package org.daisy.urakawa.exceptions;
-
-/**
- * Some methods have parameters of numeric type (float, int, uint, etc.).
- * This exception should be thrown when values are out of allowed bounds.
- */
-public class MethodParameterIsValueOutOfBoundsException extends MethodParameterIsInvalidException {
-}
