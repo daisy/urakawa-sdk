98efc32bbef661312244066d738f2c20e70168be
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-05-08 18:17:53 +0000

    - Daniel WECK * Simple reordering for diagram readability. * Updated IntelliJ IDEA project files.

--- trunk/java/UMLGraph/src/org/daisy/urakawa/coreDataModel/CoreNode.java
+++ trunk/java/UMLGraph/src/org/daisy/urakawa/coreDataModel/CoreNode.java
@@ -12,6 +12,19 @@ import org.daisy.urakawa.exceptions.MethodParameterIsNullException;
  */
 public interface CoreNode extends TreeNode, VisitableCoreNode {
     /**
+     * Sets the CoreNodeValidator.
+     * Should *only* be used at construction/initialization time (using the Factory).
+     * (visibility is "public" because it's mandatory in Interfaces, but it would rather be "package"
+     * so that only the Factory can call this method, not the end-user).
+     *
+     * @param validator cannot be null.
+     * @return If the CoreNode instance already has a CoreNodeValidator, it gets overwritten, and the method returns true. If there is no override, returns false.
+     * @stereotype Initialize
+     * @tagvalue Exceptions "MethodParameterIsNull"
+     */
+    public boolean setValidator(CoreNodeValidator validator) throws MethodParameterIsNullException;
+
+    /**
      * Sets a Property.
      * Should *only* be used at construction/initialization time (using the Factory).
      * (visibility is "public" because it's mandatory in Interfaces, but it would rather be "package"
@@ -38,8 +51,8 @@ public interface CoreNode extends TreeNode, VisitableCoreNode {
     /**
      * @param deep if true, the full tree fragment is copied and returned, including children of children, etc. recursively.
      * @return a copy of this node, which has the same Presentation instance, but has cloned Property instances.
-     * (the Composition relationship implies that the Property instances live in the life-space of this object).
-     * This applies recursively, if deep was set to true.
+     *         (the Composition relationship implies that the Property instances live in the life-space of this object).
+     *         This applies recursively, if deep was set to true.
      */
     public CoreNode copy(boolean deep);
 
@@ -47,17 +60,4 @@ public interface CoreNode extends TreeNode, VisitableCoreNode {
      * @return the Validator for this node (delegate)
      */
     public CoreNodeValidator getValidator();
-
-    /**
-     * Sets the CoreNodeValidator.
-     * Should *only* be used at construction/initialization time (using the Factory).
-     * (visibility is "public" because it's mandatory in Interfaces, but it would rather be "package"
-     * so that only the Factory can call this method, not the end-user).
-     *
-     * @param validator cannot be null.
-     * @return If the CoreNode instance already has a CoreNodeValidator, it gets overwritten, and the method returns true. If there is no override, returns false.
-     * @stereotype Initialize
-     * @tagvalue Exceptions "MethodParameterIsNull"
-     */
-    public boolean setValidator(CoreNodeValidator validator) throws MethodParameterIsNullException;
 }
