48cd4edbb9ea49ec37f4053eda3fe3cd58384e74
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-05-22 09:18:28 +0000

    Updating minor changes

--- trunk/csharp/UrakawaToolkit/IProperty.cs
+++ trunk/csharp/UrakawaToolkit/IProperty.cs
@@ -26,13 +26,23 @@ namespace urakawa.core
 	/// <summary>
 	/// Common interface for properties
 	/// </summary>
-	public interface IProperty:IXUKable
+	public interface IProperty : IXUKable
 	{
     /// <summary>
     /// Gets the <see cref="PropertyType"/> of the <see cref="IProperty"/>
     /// </summary>
     /// <returns>The <see cref="PropertyType"/></returns>
 		PropertyType getPropertyType();
+
+    /// <summary>
+    /// The actual Property object implementations must define the semantics of such copy,
+    /// as it has critical implications in terms of memory management, shared object pools, etc.
+    /// e.g.: a ChannelsProperty that has Media objects pointing to actual files (like MP3 audio files)
+    /// => Media should be sufficiently abstract and well-managed via the some sort of MediaAssetManager
+    /// to guarantee that sharing conflicts are resolved transparently.
+    /// </summary>
+    /// <returns>A copy/clone of the current instance</returns>
+    IProperty copy();
 	}
 
 
