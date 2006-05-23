3debc8716e6e852878008f744ac5ef0a574df7e9
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-05-23 09:48:17 +0000

    Added classes ChannelsPropertyCoreNodevalidator and DetectMediaCoreNodeVisitor Added canInsert method to ICoreNodeValidator

--- csharp/UrakawaToolkit/CompositeCoreNodeValidator.cs
+++ csharp/UrakawaToolkit/CompositeCoreNodeValidator.cs
@@ -5,63 +5,71 @@ namespace urakawa.core
 	/// <summary>
 	/// Summary description for CompositeCoreNodeValidator.
 	/// </summary>
-	public class CompositeCoreNodeValidator : ICoreNodeValidator
-	{
-		public CompositeCoreNodeValidator()
-		{
-			//
-			// TODO: Add constructor logic here
-			//
-		}
+  public class CompositeCoreNodeValidator : ICoreNodeValidator
+  {
+    private ICoreNodeValidator[] mValidators;
 
-		#region ICoreNodeValidator Members
+    internal CompositeCoreNodeValidator(ICoreNodeValidator[] validators)
+    {
+      mValidators = validators;
+    }
 
-		//@todo
-		//implement all of these functions
+    #region ICoreNodeValidator Members
 
-		public bool canSetProperty(IProperty newProp, ICoreNode contextNode)
-		{
-			return false;
-		}
+    //TODO: implement all of these functions
+
+    public bool canSetProperty(IProperty newProp, ICoreNode contextNode)
+    {
+      foreach (ICoreNodeValidator v in mValidators)
+      {
+        if (!v.canSetProperty(newProp, contextNode)) return false;
+      }
+      return true;
+    }
 		
-		public bool canRemoveChild(ICoreNode node)
-		{
-			return false;
-		}
+    public bool canRemoveChild(ICoreNode node)
+    {
+      return true;
+    }
+
+    public bool canInsert(ICoreNode node, int index, ICoreNode contextNode)
+    {
+      return true;
+    }
 		
 		public bool canInsertBefore(ICoreNode node, ICoreNode anchorNode)
 		{
-			return false;
+			return true;
 		}
 		
 		public bool canInsertAfter(ICoreNode node, ICoreNode anchorNode)
 		{
-			return false;
+			return true;
 		}
 
 		public bool canReplaceChild(ICoreNode node, ICoreNode oldNode)
 		{
-			return false;
+			return true;
 		}
 		
 		public bool canReplaceChild(ICoreNode node, int index, ICoreNode contextNode)
 		{
-			return false;
+			return true;
 		}
 		
 		public bool canRemoveChild(int index, ICoreNode contextNode)
 		{
-			return false;
+			return true;
 		}
 
 		public bool canAppendChild(ICoreNode node, ICoreNode contextNode)
 		{
-			return false;
+			return true;
 		}
 
 		public bool canDetach(ICoreNode contextNode)
 		{
-			return false;
+			return true;
 		}
 
 		#endregion
