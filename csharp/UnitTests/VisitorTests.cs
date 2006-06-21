9805d8f9812ba9636abb4cd752b071462c15eef7
Marisa DeMeglio <marisa.demeglio@gmail.com>
2006-06-21 05:04:53 +0000

    Added new visitor for visiting nodes based on their XML element name. This visitor needs more tweaking and feedback before it will be useful for retrieving an NCX-like structure, but that is its eventual goal.
    Added a unit test for the new visitor.

---
+++ csharp/UnitTests/VisitorTests.cs
@@ -0,0 +1,46 @@
+using System;
+using NUnit.Framework;
+using urakawa.core;
+
+namespace urakawa.test.unitTests
+{
+	/// <summary>
+	/// Summary description for VisitorTests.
+	/// </summary>
+	[TestFixture]
+	public class VisitorTests
+	{
+		private urakawa.project.Project mProject;
+		private string mDefaultFile = "../../XukWorks/SampleDTB2Ver1.xuk";
+
+		[SetUp] public void Init() 
+		{
+			mProject = new urakawa.project.Project();
+			
+			string filepath = System.IO.Directory.GetCurrentDirectory();
+
+			Uri fileUri = new Uri(filepath);
+			
+			fileUri = new Uri(fileUri, mDefaultFile);
+			
+			mProject.openXUK(fileUri);
+		}
+
+		[Test] public void VisitXmlPropertyName()
+		{
+			XmlPropertyElementNameVisitor vis = new XmlPropertyElementNameVisitor();
+
+			vis.addElementName("level");
+
+			mProject.getPresentation().getRootNode().acceptDepthFirst(vis);
+
+			System.Collections.ArrayList list = (System.Collections.ArrayList)vis.getResults();
+
+			Assert.IsNotNull(list);
+
+			//this particular file (SampleDTB2Ver1.xuk) has one level node
+			Assert.AreEqual(list.Count, 1);
+		}
+
+	}
+}
