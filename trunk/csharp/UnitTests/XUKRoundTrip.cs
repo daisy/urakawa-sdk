3349d436541397a7d9ae73b52314ff56a8bf1b60
marisademeglio <marisademeglio@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-07-07 12:13:09 +0000

    Fixed filename typo in test XUK file

--- trunk/csharp/UnitTests/XUKRoundTrip.cs
+++ trunk/csharp/UnitTests/XUKRoundTrip.cs
@@ -9,55 +9,55 @@ namespace urakawa.test.unitTests
 	/// <summary>
 	/// Tests for checking if the XUK format is "round-trip" proof,
 	/// </summary>
-  [TestFixture]
+	[TestFixture]
 	public class XUKRoundTrip
 	{
 
-    private urakawa.project.Project mProject;
-    private string mDefaultFile = "../../XukWorks/roundTrimTestSample.xuk";
+		private urakawa.project.Project mProject;
+		private string mDefaultFile = "../../XukWorks/roundTripTestSample.xuk";
 
 
 		public XUKRoundTrip()
 		{
 		}
 
-    [SetUp]
-    public void Init()
-    {
-      mProject = new urakawa.project.Project();
+		[SetUp]
+		public void Init()
+		{
+			mProject = new urakawa.project.Project();
 			
-      string filepath = Directory.GetCurrentDirectory();
+			string filepath = Directory.GetCurrentDirectory();
 
-      Uri fileUri = new Uri(filepath);
+			Uri fileUri = new Uri(filepath);
 			
-      fileUri = new Uri(fileUri, mDefaultFile);
+			fileUri = new Uri(fileUri, mDefaultFile);
 			
-      Assert.IsTrue(mProject.openXUK(fileUri), "Failed to load XUK file {0}", mDefaultFile);
-    }
+			Assert.IsTrue(mProject.openXUK(fileUri), "Failed to load XUK file {0}", mDefaultFile);
+		}
 
-    [Test]
-    public void AreRootNodesEqualAfterSaveAndReload()
-    {
-      MemoryStream memStream = new MemoryStream();
-      XmlTextWriter wr = new XmlTextWriter(memStream, System.Text.Encoding.UTF8);
-      Assert.IsTrue(mProject.getPresentation().XUKout(wr), "failed to write presentation to memory stream");
-      wr.Flush();
-      Presentation reloadedPresentation = new Presentation();
-      wr = null;
-      memStream.Position = 0;
-      XmlTextReader rd = new XmlTextReader(memStream);
-      while (rd.Read())
-      {
-        if (rd.NodeType==System.Xml.XmlNodeType.Element && rd.LocalName=="Presentation") break;
-      }
-      Assert.IsTrue(reloadedPresentation.XUKin(rd), "Failed to reload presentation from memory stream");
-      rd.Close();
-      Assert.IsTrue(
-        CoreNode.areCoreNodesEqual(
-          ((Presentation)mProject.getPresentation()).getRootNode(), 
-          reloadedPresentation.getRootNode(),
-          true),
-        "Root nodes of original and reloaded presentations are not equal");
-    }
+		[Test]
+		public void AreRootNodesEqualAfterSaveAndReload()
+		{
+			MemoryStream memStream = new MemoryStream();
+			XmlTextWriter wr = new XmlTextWriter(memStream, System.Text.Encoding.UTF8);
+			Assert.IsTrue(mProject.getPresentation().XUKout(wr), "failed to write presentation to memory stream");
+			wr.Flush();
+			Presentation reloadedPresentation = new Presentation();
+			wr = null;
+			memStream.Position = 0;
+			XmlTextReader rd = new XmlTextReader(memStream);
+			while (rd.Read())
+			{
+				if (rd.NodeType==System.Xml.XmlNodeType.Element && rd.LocalName=="Presentation") break;
+			}
+			Assert.IsTrue(reloadedPresentation.XUKin(rd), "Failed to reload presentation from memory stream");
+			rd.Close();
+			Assert.IsTrue(
+				CoreNode.areCoreNodesEqual(
+				((Presentation)mProject.getPresentation()).getRootNode(), 
+				reloadedPresentation.getRootNode(),
+				true),
+				"Root nodes of original and reloaded presentations are not equal");
+		}
 	}
 }
