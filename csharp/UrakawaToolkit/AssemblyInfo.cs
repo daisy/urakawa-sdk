9d711becacb3d72fb98c13e5e1c70bc6673e1cd1
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-08-17 21:21:16 +0000

    Changed toolkit version to 0.5

--- csharp/UrakawaToolkit/AssemblyInfo.cs
+++ csharp/UrakawaToolkit/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.CompilerServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 //
-[assembly: AssemblyTitle("")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("urakawa")]
+[assembly: AssemblyDescription("Urakawa Toolkit")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("")]
+[assembly: AssemblyProduct("Urakawa Toolkit")]
 [assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]		
@@ -26,7 +26,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.5.*")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
