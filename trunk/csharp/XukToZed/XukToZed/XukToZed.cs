6880ef36929b7e0162315080ecc395fbb04fb039
LaustN <LaustN@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-10-02 14:41:33 +0000

    Beginngs of XukToZed

---
+++ trunk/csharp/XukToZed/XukToZed/XukToZed.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Xsl;
+using System.Xml;
+using NUnit.Framework;
+
+namespace XukToZed
+{
+    [TestFixture]
+    public class XukToZedTests {
+
+        public XukToZedTests()
+        { 
+        }
+
+        [Test]
+        public void FirstTest()
+        {
+            XukToZed testObject = new XukToZed(@"C:\work in progress\XukToZed\XukToZed\XukToZed.xslt");
+            Assert.IsNotNull(testObject);
+            testObject.OuputDir = @"c:\XukToZedOut";
+
+            XmlReader testDoc = XmlReader.Create(@"C:\ObiTest\First_Obi_Test.xuk");
+
+            testObject.WriteZed(testDoc);
+
+        }
+    }
+
+
+    public class XukToZed
+    {
+        System.Xml.Xsl.XslCompiledTransform theTransformer = new XslCompiledTransform(true);
+        private string strOutputDir = ".";
+
+        public XukToZed(string pathToStylesheet)
+        {
+            theTransformer.Load(pathToStylesheet);
+        }
+
+        public string OuputDir
+        {
+            get
+            { 
+                return strOutputDir;
+            }
+            set
+            {
+                strOutputDir = value;
+            }
+        }
+
+        public void WriteZed(XmlReader input)
+        {
+            System.IO.StringWriter dataHolder = new System.IO.StringWriter();
+            XmlWriter results = XmlWriter.Create((System.IO.TextWriter)dataHolder);
+            try
+            {
+                theTransformer.Transform(input, results);
+            }
+            catch
+            { 
+                results = null;
+            }
+
+            XmlDocument resDoc = new XmlDocument();
+            resDoc.LoadXml(dataHolder.ToString());
+
+            XmlNode ncxTree = resDoc.DocumentElement.SelectSingleNode("//ncx");
+            XmlWriter ncxFile = XmlWriter.Create(strOutputDir + "/navigation.ncx");
+            ncxFile.WriteNode(ncxTree.CreateNavigator(), false);
+            ncxFile.Close();
+
+            XmlNode smilTree = resDoc.DocumentElement.SelectSingleNode("//smil");
+            XmlWriter smilFile = XmlWriter.Create(strOutputDir + "/everything.smil");
+            smilFile.WriteNode(smilTree.CreateNavigator(), false);
+            smilFile.Close();
+        }
+    }
+}
