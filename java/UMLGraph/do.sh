3e233d88894b6ecc8562cbbc98359087ba3d6e1c
Daniel Weck <625201+danielweck@users.noreply.github.com>
2006-10-04 20:30:19 +0000

    - Daniel WECK * Fixed bugs in the UML generation (Ant build file)

--- java/UMLGraph/do.sh
+++ java/UMLGraph/do.sh
@@ -14,14 +14,6 @@ sed -f dot.sed ViewExceptions.dot | cat > ViewExceptions_.dot
 sed -f dot.sed ViewVisitors.dot | cat > ViewVisitors_.dot
 sed -f dot.sed ViewCommands.dot | cat > ViewCommands_.dot
 
-sed -e 's/laquo/lt/g' -e 's/raquo/gt/g' ViewStickyNote_.dot | cat > ViewStickyNote__.dot
-sed -e 's/laquo/lt/g' -e 's/raquo/gt/g' ViewCoreDataModel_.dot | cat > ViewCoreDataModel__.dot
-sed -e 's/laquo/lt/g' -e 's/raquo/gt/g' ViewExceptions_.dot | cat > ViewExceptions__.dot
-sed -e 's/laquo/lt/g' -e 's/raquo/gt/g' ViewMedia_.dot | cat > ViewMedia__.dot
-sed -e 's/laquo/lt/g' -e 's/raquo/gt/g' ViewFullUML_.dot | cat > ViewFullUML__.dot
-sed -e 's/laquo/lt/g' -e 's/raquo/gt/g' ViewCommands_.dot | cat > ViewCommands__.dot
-sed -e 's/laquo/lt/g' -e 's/raquo/gt/g' ViewVisitors_.dot | cat > ViewVisitors__.dot
-
 # Page A4: 8.3,11.7 MARGIN: 0.5
 
 PARAMS=" -v -Tpng"
@@ -30,18 +22,18 @@ PAGESTR="_A4_Print"
 ROTATE=" -Grotate=90 "
 SIZE=" -Gsize=7.3,10.7! "
 
-dot $PARAMS -oViewVisitors$PAGESTR.$EXT ViewVisitors__.dot
-dot $PARAMS $SIZE -oViewStickyNote$PAGESTR.$EXT ViewStickyNote__.dot
-dot $PARAMS $SIZE $ROTATE -oViewFullUML$PAGESTR.$EXT ViewFullUML__.dot
-dot $PARAMS $SIZE $ROTATE -oViewCoreDataModel$PAGESTR.$EXT ViewCoreDataModel__.dot
-dot $PARAMS $SIZE $ROTATE -oViewExceptions$PAGESTR.$EXT ViewExceptions__.dot
-dot $PARAMS $SIZE $ROTATE -oViewMedia$PAGESTR.$EXT ViewMedia__.dot
-dot $PARAMS $SIZE $ROTATE -oViewCommands$PAGESTR.$EXT ViewCommands__.dot
+dot $PARAMS -oViewVisitors$PAGESTR.$EXT ViewVisitors_.dot
+dot $PARAMS $SIZE -oViewStickyNote$PAGESTR.$EXT ViewStickyNote_.dot
+dot $PARAMS $SIZE $ROTATE -oViewFullUML$PAGESTR.$EXT ViewFullUML_.dot
+dot $PARAMS $SIZE $ROTATE -oViewCoreDataModel$PAGESTR.$EXT ViewCoreDataModel_.dot
+dot $PARAMS $SIZE $ROTATE -oViewExceptions$PAGESTR.$EXT ViewExceptions_.dot
+dot $PARAMS $SIZE $ROTATE -oViewMedia$PAGESTR.$EXT ViewMedia_.dot
+dot $PARAMS $SIZE $ROTATE -oViewCommands$PAGESTR.$EXT ViewCommands_.dot
 
-dot $PARAMS -oViewVisitors.$EXT ViewVisitors__.dot
-dot $PARAMS -oViewStickyNote.$EXT ViewStickyNote__.dot
-dot $PARAMS -oViewFullUML.$EXT ViewFullUML__.dot
-dot $PARAMS -oViewCoreDataModel.$EXT ViewCoreDataModel__.dot
-dot $PARAMS -oViewExceptions.$EXT ViewExceptions__.dot
-dot $PARAMS -oViewMedia.$EXT ViewMedia__.dot
-dot $PARAMS -oViewCommands.$EXT ViewCommands__.dot
+dot $PARAMS -oViewVisitors.$EXT ViewVisitors_.dot
+dot $PARAMS -oViewStickyNote.$EXT ViewStickyNote_.dot
+dot $PARAMS -oViewFullUML.$EXT ViewFullUML_.dot
+dot $PARAMS -oViewCoreDataModel.$EXT ViewCoreDataModel_.dot
+dot $PARAMS -oViewExceptions.$EXT ViewExceptions_.dot
+dot $PARAMS -oViewMedia.$EXT ViewMedia_.dot
+dot $PARAMS -oViewCommands.$EXT ViewCommands_.dot
