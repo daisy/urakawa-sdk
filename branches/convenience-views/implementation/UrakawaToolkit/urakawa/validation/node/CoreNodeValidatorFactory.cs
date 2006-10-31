05d1d8b8310382ad09811a8a7351fe04eccdea33
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2006-10-31 13:33:43 +0000

    Moving files

---
+++ branches/convenience-views/implementation/UrakawaToolkit/urakawa/validation/node/CoreNodeValidatorFactory.cs
@@ -0,0 +1,88 @@
+using System;
+using urakawa.properties.channel;
+using urakawa.properties.xml;
+
+namespace urakawa.validation.core
+{
+	/// <summary>
+	/// Default implementation of <see cref="ICoreNodeValidatorFactory"/>
+	/// </summary>
+	public class CoreNodeValidatorFactory:ICoreNodeValidatorFactory
+	{
+    /// <summary>
+    /// Default constructor
+    /// </summary>
+		public CoreNodeValidatorFactory()
+		{
+			// Nothing in particular is done
+		}
+		#region ICoreNodeValidatorFactory Members
+
+		//this function is here to satisfy the interface requirement
+		ICoreNodeValidator ICoreNodeValidatorFactory.createNodeValidator()
+		{
+			return createNodeValidator();
+		}
+
+		/// <summary>
+		/// Creates the default validator in this implementation, 
+		/// which is a <see cref="CompositeCoreNodeValidator"/> consisting of
+		/// a <see cref="TreeNodeValidator"/>, a <see cref="XmlPropertyCoreNodeValidator"/> 
+		/// and a <see cref="ChannelsPropertyCoreNodeValidator"/>
+		/// </summary>
+		/// <returns>The default <see cref="CompositeCoreNodeValidator"/></returns>
+		public CompositeCoreNodeValidator createNodeValidator()
+		{
+			TreeNodeValidator treeNodeVal = createTreeNodeValidator();
+			XmlPropertyCoreNodeValidator xmlPropCNVal = 
+				createXmlPropertyCoreNodeValidator();
+			ChannelsPropertyCoreNodeValidator channelsPropCNVal = 
+				createChannelsPropertyCoreNodeValidator();
+
+			ICoreNodeValidator[] validators = new ICoreNodeValidator[]
+				{treeNodeVal, xmlPropCNVal, channelsPropCNVal};
+			
+			return createCustomCompositeNodeValidator(validators);
+		}
+
+    /// <summary>
+    /// Creates a custon <see cref="CompositeCoreNodeValidator"/> 
+    /// consisting of given member <see cref="ICoreNodeValidator"/>s
+    /// </summary>
+    /// <param name="arr">The array of given <see cref="ICoreNodeValidator"/>s</param>
+    /// <returns>The custon <see cref="CompositeCoreNodeValidator"/></returns>
+		public CompositeCoreNodeValidator createCustomCompositeNodeValidator(ICoreNodeValidator[] arr)
+		{
+			return new CompositeCoreNodeValidator(arr);
+		}
+
+    /// <summary>
+    /// Creates a <see cref="TreeNodeValidator"/>
+    /// </summary>
+    /// <returns>The created <see cref="TreeNodeValidator"/></returns>
+		public TreeNodeValidator createTreeNodeValidator()
+		{
+			return new TreeNodeValidator();
+		}
+
+    /// <summary>
+    /// Creates a <see cref="XmlPropertyCoreNodeValidator"/>
+    /// </summary>
+    /// <returns>The created <see cref="XmlPropertyCoreNodeValidator"/></returns>
+    public XmlPropertyCoreNodeValidator createXmlPropertyCoreNodeValidator()
+		{
+			return new XmlPropertyCoreNodeValidator();
+		}
+
+    /// <summary>
+    /// Creates a <see cref="ChannelsPropertyCoreNodeValidator"/>
+    /// </summary>
+    /// <returns>The created <see cref="ChannelsPropertyCoreNodeValidator"/></returns>
+    public ChannelsPropertyCoreNodeValidator createChannelsPropertyCoreNodeValidator()
+		{
+			return new ChannelsPropertyCoreNodeValidator();
+		}
+
+		#endregion
+	}
+}
