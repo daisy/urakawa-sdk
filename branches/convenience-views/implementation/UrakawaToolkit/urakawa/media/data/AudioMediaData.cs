e221a4612376264f00155a79a240c6fa80b805b0
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2007-01-10 08:46:31 +0000

    Ops. related to renaming asset to data

--- branches/convenience-views/implementation/UrakawaToolkit/urakawa/media/data/AudioMediaData.cs
+++ branches/convenience-views/implementation/UrakawaToolkit/urakawa/media/data/AudioMediaData.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace urakawa.media.asset
+namespace urakawa.media.data
 {
 	public abstract class AudioMediaAsset : IAudioMediaAsset
 	{
