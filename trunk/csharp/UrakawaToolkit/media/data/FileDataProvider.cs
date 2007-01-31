e4d9edf2c531625b81d2a22583a315f399d941f0
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2007-01-31 14:16:41 +0000

    Just commiting a bit of impl

--- trunk/csharp/UrakawaToolkit/media/data/FileDataProvider.cs
+++ trunk/csharp/UrakawaToolkit/media/data/FileDataProvider.cs
@@ -1,23 +1,82 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 
 namespace urakawa.media.data
 {
 	public class FileDataProvider : IDataProvider
 	{
+		protected internal FileDataProvider()
+		{
+		}
+
+		private string mRelativeFilePath;
+
+		/// <summary>
+		/// Gets the full path of the file storing the data the instance
+		/// </summary>
+		/// <returns>The full path</returns>
+		public string getFilePath()
+		{
+			return mFilePath;
+		}
+
 		#region IDataProvider Members
 
+		/// <summary>
+		/// Gets an input <see cref="Stream"/> providing read access to the <see cref="FileDataProvider"/>
+		/// </summary>
+		/// <returns>The input <see cref="Stream"/></returns>
 		public System.IO.Stream getInputStream()
 		{
-			throw new Exception("The method or operation is not implemented.");
-//TODO: Implement method
+			FileStream inputStream;
+			try
+			{
+				inputStream = new FileStream(getFilePath(), FileMode.Open, FileAccess.Read, FileShare.Read);
+			}
+			catch (Exception e)
+			{
+			}
+			return inputStream;
 		}
 
 		public System.IO.Stream getOutputStream()
 		{
 			throw new Exception("The method or operation is not implemented.");
 //TODO: Implement method
+		}
+
+
+
+		IMediaDataManager IDataProvider.getManager()
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		#endregion
+
+		#region IXukAble Members
+
+		public bool XukIn(System.Xml.XmlReader source)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+		public bool XukOut(System.Xml.XmlWriter destination)
+		{
+			throw new Exception("The method or operation is not implemented.");
+		}
+
+
+		public virtual string getXukLocalName()
+		{
+			return this.GetType().Name;
+		}
+
+		public virtual string getXukNamespaceUri()
+		{
+			return ToolkitSettings.XUK_NS;
 		}
 
 		#endregion
--- trunk/csharp/UrakawaToolkit/media/data/IDataProvider.cs
+++ trunk/csharp/UrakawaToolkit/media/data/IDataProvider.cs
@@ -2,10 +2,20 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.IO;
+using System.XmlM
+using urakawa.xuk;
+
 namespace urakawa.media.data
+
 {
-	public interface IDataProvider
+	/// <summary>
+	/// Interface for a generic <see cref="IDataProvider"/> providing access to data storage 
+	/// via input and output <see cref="Stream"/>s
+	/// </summary>
+	public interface IDataProvider : IXukAble
 	{
+		IMediaDataManager getManager();
+
 		Stream getInputStream();
 
 		Stream getOutputStream();
