d7362f98b4024578a85e9cf9eef99096f4325088
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2007-01-31 13:25:27 +0000

    Deleting obsolete branch

--- branches/convenience-views/implementation/FileIOTest/monoCompile.bat
+++
@@ -1,3 +0,0 @@
-gmcs -target:exe -out:mono/FileIOTest.exe -reference:mono/urakawa.dll AssemblyInfo.cs FileIOTest.cs
-pause
-
--- branches/convenience-views/implementation/UrakawaToolkit/media/ISrcMediaLocation.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-namespace urakawa.media
-{
-	public interface ISrcMediaLocation : IMediaLocation
-	{
-		string getSrc();
-		void setSrc(string newSrc);
-	}
-}
