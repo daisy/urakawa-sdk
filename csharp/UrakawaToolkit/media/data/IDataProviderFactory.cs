6b2a560395e4e852fef97992a0d68653e95a79d3
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2007-03-05 14:10:53 +0000

    Commiting todays implementation...

--- csharp/UrakawaToolkit/media/data/IDataProviderFactory.cs
+++ csharp/UrakawaToolkit/media/data/IDataProviderFactory.cs
@@ -22,17 +22,20 @@ namespace urakawa.media.data
 		void setDataProviderManager(IDataProviderManager mngr);
 
 		/// <summary>
-		/// Creates a <see cref="IDataProvider"/> instance of default <see cref="Type"/>
+		/// Creates a <see cref="IDataProvider"/> instance of default type for a given MIME type
 		/// </summary>
+		/// <param name="mimeType">The given MIME type</param>
 		/// <returns>The created instance</returns>
-		IDataProvider createDataProvider();
+		IDataProvider createDataProvider(string mimeType);
 
 		/// <summary>
-		/// Creates a <see cref="IDataProvider"/> instance of <see cref="Type"/> matching a given XUK QName
+		/// Creates a <see cref="IDataProvider"/> instance of type matching a given XUK QName
+		/// for a given MIME type
 		/// </summary>
+		/// <param name="mimeType">The given MIME type</param>
 		/// <param name="xukLocalName">The local name part of the given XUK QName</param>
 		/// <param name="xukNamespaceUri">The namespace uri part of the given XUK QName</param>
 		/// <returns>The created instance</returns>
-		IDataProvider createDataProvider(string xukLocalName, string xukNamespaceUri);
+		IDataProvider createDataProvider(string mimeType, string xukLocalName, string xukNamespaceUri);
 	}
 }
