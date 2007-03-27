4300fd6f443f7b7a12c0389e86ef875343f7813d
Daniel Weck <625201+danielweck@users.noreply.github.com>
2007-03-27 13:13:46 +0000

    - Daniel WECK
    * removed buggy UMLGraph4.5 lib

--- java/UMLGraph/UMLGraph-4.5/testdata/java/Irp.java
+++
@@ -1,7 +0,0 @@
-// $Id: Irp.java,v 1.1 2005/11/23 22:18:45 dds Exp $
-class Application {}
-
-class IrpApplication extends Application {}
-
-/** @depend - <friend> - IrpApplication */
-class Main {}
