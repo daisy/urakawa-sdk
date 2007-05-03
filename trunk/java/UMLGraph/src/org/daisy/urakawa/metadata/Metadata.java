bab17e76f671bb12fe886119c6e94fda76180b71
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2007-05-03 04:53:09 +0000

    - Daniel WECK NOTE: this SVN commit is done with my command line client (Mac OS X UNIX shell). My Eclipse SVN client (org.tigris.subversion) is too old apparently (Subclipse).
    * Added constraints for String parameters (not null and not empty string exceptions)
    * Synchronized with SDK implementation, in particular in respect to "name" vs "content" data constraints (e.g.: by convention, content can never be null, but can be empty)

--- trunk/java/UMLGraph/src/org/daisy/urakawa/metadata/Metadata.java
+++ trunk/java/UMLGraph/src/org/daisy/urakawa/metadata/Metadata.java
@@ -1,17 +1,91 @@
 package org.daisy.urakawa.metadata;
 
+import java.util.List;
+
+import org.daisy.urakawa.ValueEquatable;
+import org.daisy.urakawa.exceptions.MethodParameterIsEmptyStringException;
+import org.daisy.urakawa.exceptions.MethodParameterIsNullException;
 import org.daisy.urakawa.xuk.XukAble;
 
-public interface Metadata extends XukAble {
-    public String getName();
+/**
+ * Metadata
+ */
+public interface Metadata extends XukAble, ValueEquatable<Metadata> {
+	/**
+	 * @return The name of the metadata entry (cannot be null or empty)
+	 */
+	public String getName();
 
-    public void setName(String name);
+	/**
+	 * Sets the name of the metadata entry
+	 * 
+	 * @param name
+	 *            cannot be null or empty string
+	 * @throws MethodParameterIsNullException
+	 * @throws MethodParameterIsEmptyStringException
+	 * @tagvalue Exceptions "MethodParameterIsNull,
+	 *           MethodParameterIsEmptyString"
+	 */
+	public void setName(String name) throws MethodParameterIsNullException,
+			MethodParameterIsEmptyStringException;
 
-    public String getContent();
+	/**
+	 * @return convenience method for
+	 *         {@link #getOptionalAttributeValue(String name)} for name =
+	 *         "content". Cannot be null but can be empty string if no there is
+	 *         no value for "content".
+	 */
+	public String getContent();
 
-    public void setContent(String content);
+	/**
+	 * convenience method for
+	 * {@link #setOptionalAttributeValue(String name, String content)} for name =
+	 * "content".
+	 * 
+	 * @param content
+	 *            Cannot be null but can be empty string if content need to be
+	 *            reset.
+	 * @throws MethodParameterIsNullException
+	 * @tagvalue Exceptions "MethodParameterIsNull"
+	 */
+	public void setContent(String content)
+			throws MethodParameterIsNullException;
 
-    public String getScheme();
+	/**
+	 * @param name
+	 *            The name for which to get the value. Cannot be null or empty
+	 *            string.
+	 * @return The value for the given name. Cannot be null but can be empty
+	 *         string if no value is set for the given name.
+	 * @throws MethodParameterIsNullException
+	 * @throws MethodParameterIsEmptyStringException
+	 * @tagvalue Exceptions "MethodParameterIsNull,
+	 *           MethodParameterIsEmptyString"
+	 */
+	public String getOptionalAttributeValue(String name)
+			throws MethodParameterIsNullException,
+			MethodParameterIsEmptyStringException;
 
-    public void setScheme(String scheme);
+	/**
+	 * 
+	 * @param name
+	 *            The name for which to set the value. Cannot be null or empty
+	 *            string.
+	 * @param content
+	 *            The value for the given name. Cannot be null but can be empty
+	 *            string if content need to be reset.
+	 * @throws MethodParameterIsNullException
+	 * @throws MethodParameterIsEmptyStringException
+	 * @tagvalue Exceptions "MethodParameterIsNull,
+	 *           MethodParameterIsEmptyString"
+	 */
+	public void setOptionalAttributeValue(String name, String content)
+			throws MethodParameterIsNullException,
+			MethodParameterIsEmptyStringException;
+
+	/**
+	 * @return the list of all set attribute names (which are strings, non
+	 *         empty, non-null). Cannot be null, but can be an empty list.
+	 */
+	List<String> getOptionalAttributeNames();
 }
