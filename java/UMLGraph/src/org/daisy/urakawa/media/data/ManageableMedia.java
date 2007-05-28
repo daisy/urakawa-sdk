6117fcaa752cd040af079f2b7be643955e78adbd
Daniel Weck <625201+danielweck@users.noreply.github.com>
2007-05-28 08:26:30 +0000

    - Daniel WECK * Updated following latest Ole's correction of getListOfUsed... DataProviders instead of MediaDatas.

--- java/UMLGraph/src/org/daisy/urakawa/media/data/ManageableMedia.java
+++ java/UMLGraph/src/org/daisy/urakawa/media/data/ManageableMedia.java
@@ -1,9 +1,9 @@
 package org.daisy.urakawa.media.data;
 
-//import java.util.List;
+import java.util.List;
 
 public interface ManageableMedia {
 	public MediaData getMediaData();
 	public void setMediaData(MediaData data);
-	//public List<MediaData> getListOfUsedMediaData();
+	List<DataProvider> getListOfUsedDataProviders();
 }
