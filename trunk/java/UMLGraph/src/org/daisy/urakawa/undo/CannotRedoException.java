466f9ed7536441fea7b1ddbac4c0b285c6319535
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2007-05-30 01:27:35 +0000

    ok, we're getting there now....

---
+++ trunk/java/UMLGraph/src/org/daisy/urakawa/undo/CannotRedoException.java
@@ -0,0 +1,14 @@
+package org.daisy.urakawa.undo;
+
+import org.daisy.urakawa.exception.CheckedException;
+
+/**
+ *
+ */
+public class CannotRedoException extends CheckedException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3431387950719820251L;
+}
