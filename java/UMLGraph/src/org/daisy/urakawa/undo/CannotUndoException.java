d90899c2628a0b521af0e3ade0d16735cb73fed3
Daniel Weck <625201+danielweck@users.noreply.github.com>
2007-05-30 01:27:35 +0000

    ok, we're getting there now....

---
+++ java/UMLGraph/src/org/daisy/urakawa/undo/CannotUndoException.java
@@ -0,0 +1,14 @@
+package org.daisy.urakawa.undo;
+
+import org.daisy.urakawa.exception.CheckedException;
+
+/**
+ *
+ */
+public class CannotUndoException extends CheckedException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 99292103792953401L;
+}
