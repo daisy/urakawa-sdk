55e818273c4827975c3bba42fdd5104cc7072249
Daniel Weck <625201+danielweck@users.noreply.github.com>
2007-06-01 22:55:46 +0000

    finishing up the media.data.audio re-organization

--- java/UMLGraph/src/org/daisy/urakawa/media/data/audio/codec/WavAudioMediaData.java
+++ java/UMLGraph/src/org/daisy/urakawa/media/data/audio/codec/WavAudioMediaData.java
@@ -1,4 +1,4 @@
-package org.daisy.urakawa.media.data.codec.audio;
+package org.daisy.urakawa.media.data.audio.codec;
 
 import java.io.InputStream;
 import java.util.List;
@@ -19,85 +19,70 @@ import org.daisy.urakawa.media.timing.TimeDelta;
  * 
  * @todo verify / add comments and exceptions
  */
-public class Mp3AudioMediaData extends AudioMediaDataAbstractImpl {
+public class WavAudioMediaData extends AudioMediaDataAbstractImpl {
 	@Override
 	protected AudioMediaData audioMediaDataCopy() {
-		
 		return null;
 	}
 
 	@Override
 	public InputStream getAudioData(Time clipBegin, Time clipEnd) {
-		
 		return null;
 	}
 
 	@Override
 	public TimeDelta getAudioDuration() {
-		
 		return null;
 	}
 
 	@Override
 	public void insertAudioData(InputStream pcmData, Time insertPoint,
 			TimeDelta duration) {
-		
 	}
 
 	@Override
 	public void removeAudio(Time clipBegin, Time clipEnd) {
-		
 	}
 
 	@Override
 	public void replaceAudioData(InputStream pcmData, Time replacePoint,
 			TimeDelta duration) {
-		
 	}
 
 	@Override
 	public boolean ValueEquals(MediaData other) {
-		
 		return false;
 	}
 
 	@Override
 	public boolean XukIn(XmlDataReader source) {
-		
 		return false;
 	}
 
 	@Override
 	public boolean XukOut(XmlDataWriter destination) {
-		
 		return false;
 	}
 
 	@Override
 	public List<DataProvider> getUsedDataProviders() {
-		
 		return null;
 	}
 
 	public List<DataProvider> getListOfUsedDataProviders() {
-		
 		return null;
 	}
 
 	public String getXukNamespaceURI() {
-		
 		return null;
 	}
 
 	@Override
 	public String getXukLocalName() {
-		
 		return null;
 	}
 
 	public void setMediaDataFactory(MediaDataFactory factory)
 			throws MethodParameterIsNullException {
-		
-		
 	}
 }
