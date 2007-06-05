a3e9f8fbff9e034bf2bf11384bee993159ac5997
Daniel Weck <625201+danielweck@users.noreply.github.com>
2007-06-05 19:13:07 +0000

    - Daniel WECK * More improvements to the Class Diagrams.

--- java/UMLGraph/src/org/daisy/urakawa/core/visitor/TreeNodeVisitor.java
+++ java/UMLGraph/src/org/daisy/urakawa/core/visitor/TreeNodeVisitor.java
@@ -15,7 +15,7 @@ import org.daisy.urakawa.exception.MethodParameterIsNullException;
  * http://www.patterndepot.com/put/8/JavaPatterns.htm
  * http://www.patterndepot.com/put/8/visitor.pdf
  *
- * @depend - - - org.daisy.urakawa.core.TreeNode
+ * @stereotype ApplicationImplemented
  */
 public interface TreeNodeVisitor {
     /**
