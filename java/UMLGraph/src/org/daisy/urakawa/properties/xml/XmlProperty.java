561635c5f60cdaddee1a908ae8737dae74816661
Daniel Weck <625201+danielweck@users.noreply.github.com>
2007-06-28 10:19:25 +0000

    - Daniel WECK * Remove XmlType, because XmlProperty is always an XML ELEMENT !

--- java/UMLGraph/src/org/daisy/urakawa/properties/xml/XmlProperty.java
+++ java/UMLGraph/src/org/daisy/urakawa/properties/xml/XmlProperty.java
@@ -4,18 +4,17 @@ import org.daisy.urakawa.core.property.Property;
 
 /**
  * <p>
- * This is a specific type of node Property that implements an XML element
- * (potentially with attributes) or XML text node.
+ * This is a specific type of node Property that implements an XML element (with
+ * support for attributes).
  * </p>
  * 
  * @leafInterface see {@link org.daisy.urakawa.LeafInterface}
  * @see org.daisy.urakawa.LeafInterface
  * @stereotype OptionalLeafInterface
- * @depend - Aggregation 1 org.daisy.urakawa.properties.xml.XmlType
  * @depend - Composition 0..n org.daisy.urakawa.properties.xml.XmlAttribute
  * @depend - Clone - org.daisy.urakawa.properties.xml.XmlProperty
  */
-public interface XmlProperty extends Property, WithXmlType, WithXmlAttributes,
+public interface XmlProperty extends Property, WithXmlAttributes,
 		WithQualifiedName {
 	/**
 	 * <p>
