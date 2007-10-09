c6caaf8e542512fc22d8c3a77e99ab994a3545e6
oleha <oleha@6064ade6-bb1e-4b30-8970-879fdf51070b>
2007-10-09 14:06:20 +0000

    More unittesting

--- trunk/csharp/UnitTests/media/ExternalTextMediaTests.cs
+++ trunk/csharp/UnitTests/media/ExternalTextMediaTests.cs
@@ -21,6 +21,7 @@ namespace urakawa.media
 		public void setUp()
 		{
 			mProject = new Project();
+			mPresentation.setRootUri(ProjectTests.SampleXukFileDirectoryUri);
 			mMedia1 = mPresentation.getMediaFactory().createMedia(typeof(ExternalTextMedia).Name, ToolkitSettings.XUK_NS) as ExternalTextMedia;
 			Assert.IsNotNull(mMedia1, "The MediaFactory could not create a {1}:{0}", typeof(ExternalTextMedia).Name, ToolkitSettings.XUK_NS);
 			mMedia2 = mPresentation.getMediaFactory().createMedia(typeof(ExternalTextMedia).Name, ToolkitSettings.XUK_NS) as ExternalTextMedia;
@@ -56,5 +57,73 @@ namespace urakawa.media
 			mMedia1.setSrc("http://localhost/test.txt");
 			IMediaTestUtils.export_valueEqualsPresentationsOk(mMedia1, destPres);
 		}
+
+		[Test, Description("Testing getText with local relative src")]
+		public void getText_localSrc()
+		{
+			mMedia1.setSrc("temp.txt");
+			Uri file = new Uri(mPresentation.getRootUri(), mMedia1.getSrc());
+			System.IO.StreamWriter wr = new System.IO.StreamWriter(file.LocalPath, false);
+			string text = "getText: Test line Ê¯Â∆ÿ≈@£$Ä\nSecond test line\tincluding a tab";
+			try
+			{
+				wr.Write(text);
+			}
+			finally
+			{
+				wr.Close();
+			}
+			Assert.IsTrue(
+				mMedia1.getText().StartsWith(text),
+				"xuk.xsd does not as expected start with:\n{0}",
+				text);
+			if (System.IO.File.Exists(file.LocalPath)) System.IO.File.Delete(file.LocalPath);
+		}
+
+		[Test]
+		[Description("Tests getText with an external http src")]
+		[Explicit("Requires being online and takes a bit of time especially on slow connections")]
+		public void getText_httpSrc()
+		{
+			mMedia1.setSrc("http://www.daisy.org/z3986/2005/ncx-2005-1.dtd");
+			string expectedStart = "<!-- NCX 2005-1 DTD  2005-06-26";
+			Assert.IsTrue(
+				mMedia1.getText().StartsWith(expectedStart), 
+				"xuk.xsd does not as expected start with:\n{0}", 
+				expectedStart);
+		}
+
+		[Test]
+		[Description(
+			"Tests that getText throws an exception.DataFileDoesNotExistException "
+			+"when the references text file does not exist")]
+		[ExpectedException(typeof(exception.CannotReadFromExternalFileException))]
+		public void getText_invalidSrc()
+		{
+			mMedia1.setSrc("filedoesnotexist.txt");
+			mMedia1.getText();
+		}
+
+		[Test, Description("Testing setText with local relative src")]
+		public void setText_localSrc()
+		{
+			mMedia1.setSrc("temp.txt");
+			string text = "setText: Test line Ê¯Â∆ÿ≈@£$Ä\nSecond test line\tincluding a tab";
+			mMedia1.setText(text);
+			Assert.AreEqual(text, mMedia1.getText(), "The ExternalTextMedia did not return the expected text");
+			Uri file = new Uri(mPresentation.getRootUri(), mMedia1.getSrc());
+			Assert.IsTrue(System.IO.File.Exists(file.LocalPath), "The file '{0}' containing the data does not exist", file.LocalPath);
+			System.IO.File.Delete(file.LocalPath);
+		}
+
+		[Test, Description("Testing setText with external http src - expected to throw an exception")]
+		[ExpectedException(typeof(exception.CannotWriteToExternalFileException))]
+		public void setText_httpSrc()
+		{
+			mMedia1.setSrc("http://www.daisy.org/z3986/2005/ncx-2005-1.dtd");
+			mMedia1.setText("Text to replace ncx version 2005-1 DTD");
+
+		}
+
 	}
 }
