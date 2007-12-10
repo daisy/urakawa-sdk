75b838c91802765e5cb1363dc64ac68b53e6ff99
Daniel Weck <625201+danielweck@users.noreply.github.com>
2007-12-10 18:30:10 +0000

    - Daniel WECK * Half-way commit: Project implementation...and some side-effect code.

---
+++ java/UMLGraph/src/org/daisy/urakawa/exception/IsAlreadyManagerOfException.java
@@ -0,0 +1,11 @@
+package org.daisy.urakawa.exception;
+
+/**
+ * Thrown when an attempt is made to register an object that is already managed.
+ */
+public class IsAlreadyManagerOfException extends CheckedException {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -3136537250757121361L;
+}
