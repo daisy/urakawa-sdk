fb08243f0a43f23dca50c5c65c8df21ad08b9dbe
daniel_weck <daniel_weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2007-12-17 16:15:12 +0000

    - Daniel WECK * Progress in media (not media.data). Fixed a few differences with the C# implementation of the API.

--- trunk/java/UMLGraph/src/org/daisy/urakawa/media/SequenceMediaImpl.java
+++ trunk/java/UMLGraph/src/org/daisy/urakawa/media/SequenceMediaImpl.java
@@ -1,15 +1,18 @@
 package org.daisy.urakawa.media;
 
 import java.net.URI;
+import java.util.LinkedList;
 import java.util.List;
 
 import org.daisy.urakawa.FactoryCannotCreateTypeException;
 import org.daisy.urakawa.Presentation;
+import org.daisy.urakawa.exception.IsNotInitializedException;
 import org.daisy.urakawa.exception.MethodParameterIsEmptyStringException;
 import org.daisy.urakawa.exception.MethodParameterIsNullException;
 import org.daisy.urakawa.exception.MethodParameterIsOutOfBoundsException;
 import org.daisy.urakawa.xuk.XmlDataReader;
 import org.daisy.urakawa.xuk.XmlDataWriter;
+import org.daisy.urakawa.xuk.XukAbleImpl;
 import org.daisy.urakawa.xuk.XukDeserializationFailedException;
 import org.daisy.urakawa.xuk.XukSerializationFailedException;
 
@@ -19,114 +22,406 @@ import org.daisy.urakawa.xuk.XukSerializationFailedException;
  * @leafInterface see {@link org.daisy.urakawa.LeafInterface}
  * @see org.daisy.urakawa.LeafInterface
  */
-public class SequenceMediaImpl implements SequenceMedia {
-	public void appendItem(Media newItem)
-			throws MethodParameterIsNullException, DoesNotAcceptMediaException {
-	}
+public class SequenceMediaImpl extends MediaAbstractImpl implements
+		SequenceMedia {
+	private List<Media> mSequence;
+	private boolean mAllowMultipleTypes;
 
-	public int getCount() {
-		return 0;
+	/**
+	 * 
+	 */
+	public SequenceMediaImpl() {
+		mSequence = new LinkedList<Media>();
+		mAllowMultipleTypes = false;
 	}
 
 	public Media getItem(int index)
 			throws MethodParameterIsOutOfBoundsException {
-		return null;
-	}
-
-	public List<Media> getListOfItems() {
-		return null;
+		if (0 <= index && index < getCount()) {
+			return (Media) mSequence.get(index);
+		} else {
+			throw new MethodParameterIsOutOfBoundsException();
+		}
 	}
 
 	public void insertItem(int index, Media newItem)
 			throws MethodParameterIsNullException,
 			MethodParameterIsOutOfBoundsException, DoesNotAcceptMediaException {
+		if (newItem == null) {
+			throw new MethodParameterIsNullException();
+		}
+		if (index < 0 || getCount() < index) {
+			throw new MethodParameterIsOutOfBoundsException();
+		}
+		if (!canAcceptMedia(newItem)) {
+			throw new DoesNotAcceptMediaException();
+		}
+		mSequence.add(index, newItem);
+	}
+
+	public void appendItem(Media newItem)
+			throws MethodParameterIsNullException, DoesNotAcceptMediaException {
+		if (newItem == null) {
+			throw new MethodParameterIsNullException();
+		}
+		try {
+			insertItem(getCount(), newItem);
+		} catch (MethodParameterIsOutOfBoundsException e) {
+			// Should never happen
+			throw new RuntimeException("WFT ??!", e);
+		}
 	}
 
 	public Media removeItem(int index)
 			throws MethodParameterIsOutOfBoundsException {
-		return null;
+		Media removedMedia = getItem(index);
+		try {
+			removeItem(removedMedia);
+		} catch (MethodParameterIsNullException e) {
+			// Should never happen
+			throw new RuntimeException("WFT ??!", e);
+		} catch (MediaIsNotInSequenceException e) {
+			// Should never happen
+			throw new RuntimeException("WFT ??!", e);
+		}
+		return removedMedia;
 	}
 
-	public Media copy() {
-		return null;
+	public void removeItem(Media item) throws MethodParameterIsNullException,
+			MediaIsNotInSequenceException {
+		if (item == null) {
+			throw new MethodParameterIsNullException();
+		}
+		if (!mSequence.contains(item)) {
+			throw new MediaIsNotInSequenceException();
+		}
+		mSequence.remove(item);
 	}
 
+	public int getCount() {
+		return mSequence.size();
+	}
+
+	public List<Media> getListOfItems() {
+		return new LinkedList<Media>(mSequence);
+	}
+
+	public boolean getAllowMultipleTypes() {
+		return mAllowMultipleTypes;
+	}
+
+	@SuppressWarnings("unchecked")
+	public void setAllowMultipleTypes(boolean newValue)
+			throws SequenceHasMultipleTypesException {
+		if (!newValue) {
+			int count = getCount();
+			if (count > 0) {
+				Class<Media> firstItemType;
+				try {
+					firstItemType = (Class<Media>) getItem(0).getClass();
+				} catch (MethodParameterIsOutOfBoundsException e) {
+					// Should never happen
+					throw new RuntimeException("WFT ??!", e);
+				}
+				int i = 1;
+				while (i < count) {
+					try {
+						if (getItem(i).getClass() != firstItemType) {
+							throw new SequenceHasMultipleTypesException();
+						}
+					} catch (MethodParameterIsOutOfBoundsException e) {
+						// Should never happen
+						throw new RuntimeException("WFT ??!", e);
+					}
+				}
+			}
+		}
+		mAllowMultipleTypes = newValue;
+	}
+
+	@Override
 	public boolean isContinuous() {
-		return false;
+		if (getCount() > 0) {
+			try {
+				return getItem(0).isContinuous();
+			} catch (MethodParameterIsOutOfBoundsException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			}
+		} else {
+			return false;
+		}
 	}
 
+	@Override
 	public boolean isDiscrete() {
-		return false;
+		// use the first item in the collection to determine the value
+		if (getCount() > 0) {
+			try {
+				return getItem(0).isDiscrete();
+			} catch (MethodParameterIsOutOfBoundsException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			}
+		} else {
+			return false;
+		}
 	}
 
+	@Override
 	public boolean isSequence() {
-		return false;
+		return true;
 	}
 
-	public MediaFactory getMediaFactory() {
-		return null;
+	@Override
+	public SequenceMedia copy() {
+		return (SequenceMedia) copyProtected();
 	}
 
-	public void setMediaFactory(MediaFactory factory)
-			throws MethodParameterIsNullException {
+	@Override
+	protected Media copyProtected() {
+		Media newMedia;
+		try {
+			newMedia = getMediaFactory().createMedia(getXukLocalName(),
+					getXukNamespaceURI());
+		} catch (MethodParameterIsNullException e) {
+			// Should never happen
+			throw new RuntimeException("WFT ??!", e);
+		} catch (MethodParameterIsEmptyStringException e) {
+			// Should never happen
+			throw new RuntimeException("WFT ??!", e);
+		}
+		SequenceMedia newSeqMedia = (SequenceMedia) newMedia;
+		for (Media item : getListOfItems()) {
+			try {
+				newSeqMedia.appendItem(item.copy());
+			} catch (MethodParameterIsNullException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			} catch (DoesNotAcceptMediaException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			}
+		}
+		return newSeqMedia;
 	}
 
-	public String getXukLocalName() {
-		return null;
-	}
-
-	public String getXukNamespaceURI() {
-		return null;
-	}
-
-	public boolean ValueEquals(Media other)
-			throws MethodParameterIsNullException {
-		return false;
-	}
-
-	public void XukIn(XmlDataReader source)
-			throws MethodParameterIsNullException,
-			XukDeserializationFailedException {
-	}
-
-	public void XukOut(XmlDataWriter destination, URI baseURI)
-			throws MethodParameterIsNullException,
-			XukSerializationFailedException {
-	}
-
-	public String getLanguage() {
-		return null;
-	}
-
-	public void setLanguage(String name)
-			throws MethodParameterIsEmptyStringException {
-	}
-
-	public Media export(Presentation destPres)
+	@Override
+	public SequenceMedia export(Presentation destPres)
 			throws FactoryCannotCreateTypeException,
 			MethodParameterIsNullException {
-		return null;
+		if (destPres == null) {
+			throw new MethodParameterIsNullException();
+		}
+		return (SequenceMedia) exportProtected(destPres);
 	}
 
-	public boolean canAcceptMedia(Media media) {
-		return false;
+	@Override
+	protected Media exportProtected(Presentation destPres)
+			throws FactoryCannotCreateTypeException,
+			MethodParameterIsNullException {
+		if (destPres == null) {
+			throw new MethodParameterIsNullException();
+		}
+		SequenceMedia exported;
+		try {
+			exported = (SequenceMedia) destPres.getMediaFactory().createMedia(
+					getXukLocalName(), getXukNamespaceURI());
+		} catch (MethodParameterIsEmptyStringException e) {
+			// Should never happen
+			throw new RuntimeException("WFT ??!", e);
+		} catch (IsNotInitializedException e) {
+			// Should never happen
+			throw new RuntimeException("WFT ??!", e);
+		}
+		if (exported == null) {
+			throw new FactoryCannotCreateTypeException();
+		}
+		for (Media m : getListOfItems()) {
+			try {
+				exported.appendItem(m.export(destPres));
+			} catch (DoesNotAcceptMediaException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			}
+		}
+		return exported;
 	}
 
-	public void xukIn(XmlDataReader source)
+	public boolean canAcceptMedia(Media proposedAddition)
+			throws MethodParameterIsNullException {
+		if (proposedAddition == null) {
+			throw new MethodParameterIsNullException();
+		}
+		if (getCount() > 0 && !getAllowMultipleTypes()) {
+			try {
+				if (getItem(0).getClass() != proposedAddition.getClass())
+					return false;
+			} catch (MethodParameterIsOutOfBoundsException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			}
+		}
+		return true;
+	}
+
+	@Override
+	protected void clear() {
+		mAllowMultipleTypes = false;
+		for (Media item : getListOfItems()) {
+			try {
+				removeItem(item);
+			} catch (MethodParameterIsNullException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			} catch (MediaIsNotInSequenceException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			}
+		}
+		super.clear();
+	}
+
+	@Override
+	protected void xukInAttributes(XmlDataReader source)
 			throws MethodParameterIsNullException,
 			XukDeserializationFailedException {
+		if (source == null) {
+			throw new MethodParameterIsNullException();
+		}
+		String val = source.getAttribute("allowMultipleMediaTypes");
+		if (val == "true" || val == "1") {
+			try {
+				setAllowMultipleTypes(true);
+			} catch (SequenceHasMultipleTypesException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			}
+		} else {
+			try {
+				setAllowMultipleTypes(false);
+			} catch (SequenceHasMultipleTypesException e) {
+				throw new XukDeserializationFailedException();
+			}
+		}
+		super.xukInAttributes(source);
 	}
 
-	public void xukOut(XmlDataWriter destination, URI baseURI)
+	@Override
+	protected void xukInChild(XmlDataReader source)
+			throws MethodParameterIsNullException,
+			XukDeserializationFailedException {
+		if (source == null) {
+			throw new MethodParameterIsNullException();
+		}
+		boolean readItem = false;
+		if (source.getNamespaceURI() == XukAbleImpl.XUK_NS) {
+			readItem = true;
+			if (source.getLocalName() == "mSequence") {
+				xukInSequence(source);
+			} else {
+				readItem = false;
+			}
+		}
+		if (!readItem)
+			super.xukIn(source);
+	}
+
+	private void xukInSequence(XmlDataReader source)
+			throws MethodParameterIsNullException,
+			XukDeserializationFailedException {
+		if (source == null) {
+			throw new MethodParameterIsNullException();
+		}
+		if (!source.isEmptyElement()) {
+			while (source.read()) {
+				if (source.getNodeType() == XmlDataReader.ELEMENT) {
+					Media newMedia;
+					try {
+						newMedia = getMediaFactory()
+								.createMedia(source.getLocalName(),
+										source.getNamespaceURI());
+					} catch (MethodParameterIsEmptyStringException e) {
+						// Should never happen
+						throw new RuntimeException("WFT ??!", e);
+					}
+					if (newMedia != null) {
+						newMedia.xukIn(source);
+						if (!canAcceptMedia(newMedia)) {
+							throw new XukDeserializationFailedException();
+						}
+						try {
+							insertItem(getCount(), newMedia);
+						} catch (MethodParameterIsOutOfBoundsException e) {
+							// Should never happen
+							throw new RuntimeException("WFT ??!", e);
+						} catch (DoesNotAcceptMediaException e) {
+							// Should never happen
+							throw new RuntimeException("WFT ??!", e);
+						}
+					} else if (!source.isEmptyElement()) {
+						source.readSubtree().close();
+					}
+				} else if (source.getNodeType() == XmlDataReader.END_ELEMENT) {
+					break;
+				}
+				if (source.isEOF())
+					throw new XukDeserializationFailedException();
+			}
+		}
+	}
+
+	@Override
+	protected void xukOutAttributes(XmlDataWriter destination, URI baseUri)
 			throws MethodParameterIsNullException,
 			XukSerializationFailedException {
+		if (destination == null || baseUri == null) {
+			throw new MethodParameterIsNullException();
+		}
+		destination.writeAttributeString("allowMultipleMediaTypes",
+				getAllowMultipleTypes() ? "true" : "false");
+		super.xukOutAttributes(destination, baseUri);
 	}
 
-	public Presentation getPresentation() {
-		return null;
+	@Override
+	protected void xukOutChildren(XmlDataWriter destination, URI baseUri)
+			throws MethodParameterIsNullException,
+			XukSerializationFailedException {
+		if (destination == null || baseUri == null) {
+			throw new MethodParameterIsNullException();
+		}
+		if (getCount() > 0) {
+			destination.writeStartElement("mSequence", XukAbleImpl.XUK_NS);
+			for (int i = 0; i < getCount(); i++) {
+				try {
+					getItem(i).xukOut(destination, baseUri);
+				} catch (MethodParameterIsOutOfBoundsException e) {
+					// Should never happen
+					throw new RuntimeException("WFT ??!", e);
+				}
+			}
+			destination.writeEndElement();
+		}
+		super.xukOutChildren(destination, baseUri);
 	}
 
-	public void setPresentation(Presentation presentation)
+	@Override
+	public boolean ValueEquals(Media other)
 			throws MethodParameterIsNullException {
+		if (!super.ValueEquals(other))
+			return false;
+		SequenceMedia otherSeq = (SequenceMedia) other;
+		if (getCount() != otherSeq.getCount())
+			return false;
+		for (int i = 0; i < getCount(); i++) {
+			try {
+				if (!getItem(i).ValueEquals(otherSeq.getItem(i)))
+					return false;
+			} catch (MethodParameterIsOutOfBoundsException e) {
+				// Should never happen
+				throw new RuntimeException("WFT ??!", e);
+			}
+		}
+		return true;
 	}
 }
