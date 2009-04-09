e55dee467304a7094e44b3bb82dfb9364474bc7d
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-04-09 10:40:00 +0000

    adding NAudio source.

---
+++ trunk/csharp/audio/NAudio/Mixer/MixerLine.cs
@@ -0,0 +1,315 @@
+// created on 10/12/2002 at 20:37
+using System;
+using System.Runtime.InteropServices;
+using NAudio.Wave;
+using System.Collections.Generic;
+
+namespace NAudio.Mixer 
+{
+	/// <summary>
+	/// Represents a mixer line (source or destination)
+	/// </summary>
+	public class MixerLine 
+	{
+		private MixerInterop.MIXERLINE mixerLine;
+        private IntPtr mixerHandle;
+		private int destinationIndex;
+        private int sourceIndex;
+        private MixerFlags mixerHandleType;
+
+		/// <summary>
+		/// Creates a new mixer destination
+		/// </summary>
+        /// <param name="mixerHandle">Mixer Handle</param>
+        /// <param name="destinationIndex">Destination Index</param>
+        public MixerLine(IntPtr mixerHandle, int destinationIndex, MixerFlags mixerHandleType) 
+		{
+			mixerLine = new MixerInterop.MIXERLINE();			
+			mixerLine.cbStruct = Marshal.SizeOf(mixerLine);
+			mixerLine.dwDestination = destinationIndex;
+            MmException.Try(MixerInterop.mixerGetLineInfo(mixerHandle, ref mixerLine, mixerHandleType | MixerFlags.GetLineInfoOfDestination), "mixerGetLineInfo");
+            this.mixerHandle = mixerHandle;
+            this.mixerHandleType = mixerHandleType;
+			this.destinationIndex = destinationIndex;            
+		}
+
+        /// <summary>
+		/// Creates a new Mixer Source
+		/// </summary>
+        /// <param name="mixerHandle">Mixer Handle</param>
+		/// <param name="nDestination">Destination ID</param>
+		/// <param name="nSource">Source ID</param>
+        /// <param name="mixerHandleType">Flag indicating the meaning of mixerHandle</param>
+        public MixerLine(IntPtr mixerHandle, int nDestination, int nSource, MixerFlags mixerHandleType) 
+		{
+			mixerLine = new MixerInterop.MIXERLINE();
+			mixerLine.cbStruct = Marshal.SizeOf(mixerLine);
+			mixerLine.dwDestination = nDestination;
+			mixerLine.dwSource = nSource;
+            this.mixerHandleType = mixerHandleType;
+            MmException.Try(MixerInterop.mixerGetLineInfo(mixerHandle, ref mixerLine, mixerHandleType | MixerFlags.GetLineInfoOfSource), "mixerGetLineInfo");
+            this.mixerHandle = mixerHandle;
+			this.destinationIndex = nDestination;
+			this.sourceIndex = nSource;            
+		}
+
+        private MixerLine()
+        {
+        }
+
+        /// <summary>
+        /// Creates a new Mixer Source
+        /// </summary>
+        /// <param name="waveInDevice">Wave In Device</param>
+        public static int GetMixerIdForWaveIn(int waveInDevice)
+        {
+            int mixerId = -1;
+            MmException.Try(MixerInterop.mixerGetID((IntPtr)waveInDevice, out mixerId, MixerFlags.WaveIn), "mixerGetID");
+            return mixerId;
+        }
+
+		/// <summary>
+		/// Mixer Line Name
+		/// </summary>
+		public String Name 
+		{
+			get 
+			{
+				return mixerLine.szName;
+			}
+		}
+		
+		/// <summary>
+		/// Mixer Line short name
+		/// </summary>
+		public String ShortName 
+		{
+			get 
+			{
+				return mixerLine.szShortName;
+			}
+		}
+
+        /// <summary>
+        /// The line ID
+        /// </summary>
+        public int LineId
+        {
+            get
+            {
+                return mixerLine.dwLineID;
+            }
+        }
+
+        /// <summary>
+        /// Component Type
+        /// </summary>
+        public MixerLineComponentType ComponentType
+        {
+            get
+            {
+                return mixerLine.dwComponentType;
+            }
+        }
+
+		/// <summary>
+		/// Mixer destination type description
+		/// </summary>
+		public String TypeDescription 
+		{
+			get 
+			{
+                switch (mixerLine.dwComponentType)
+                {
+                    // destinations
+                    case MixerLineComponentType.DestinationUndefined:
+                        return "Undefined Destination";
+                    case MixerLineComponentType.DestinationDigital:
+                        return "Digital Destination";
+                    case MixerLineComponentType.DestinationLine:
+                        return "Line Level Destination";
+                    case MixerLineComponentType.DestinationMonitor:
+                        return "Monitor Destination";
+                    case MixerLineComponentType.DestinationSpeakers:
+                        return "Speakers Destination";
+                    case MixerLineComponentType.DestinationHeadphones:
+                        return "Headphones Destination";
+                    case MixerLineComponentType.DestinationTelephone:
+                        return "Telephone Destination";
+                    case MixerLineComponentType.DestinationWaveIn:
+                        return "Wave Input Destination";
+                    case MixerLineComponentType.DestinationVoiceIn:
+                        return "Voice Recognition Destination";
+                    // sources
+                    case MixerLineComponentType.SourceUndefined:
+                        return "Undefined Source";
+                    case MixerLineComponentType.SourceDigital:
+                        return "Digital Source";
+                    case MixerLineComponentType.SourceLine:
+                        return "Line Level Source";
+                    case MixerLineComponentType.SourceMicrophone:
+                        return "Microphone Source";
+                    case MixerLineComponentType.SourceSynthesizer:
+                        return "Synthesizer Source";
+                    case MixerLineComponentType.SourceCompactDisc:
+                        return "Compact Disk Source";
+                    case MixerLineComponentType.SourceTelephone:
+                        return "Telephone Source";
+                    case MixerLineComponentType.SourcePcSpeaker:
+                        return "PC Speaker Source";
+                    case MixerLineComponentType.SourceWaveOut:
+                        return "Wave Out Source";
+                    case MixerLineComponentType.SourceAuxiliary:
+                        return "Auxiliary Source";
+                    case MixerLineComponentType.SourceAnalog:
+                        return "Analog Source";
+                    default:
+                        return "Invalid Component Type";
+                }
+			}				
+		}
+		
+		/// <summary>
+		/// Number of channels
+		/// </summary>
+		public int Channels 
+		{
+			get 
+			{
+				return mixerLine.cChannels;
+			}
+		}
+		
+		/// <summary>
+		/// Number of sources
+		/// </summary>
+		public int SourceCount 
+		{
+			get 
+			{
+				return mixerLine.cConnections;
+			}
+		}
+		
+		/// <summary>
+		/// Number of controls
+		/// </summary>
+		public int ControlsCount 
+		{
+			get 
+			{
+				return mixerLine.cControls;
+			}
+		}
+
+        /// <summary>
+        /// Is this destination active
+        /// </summary>
+        public bool IsActive
+        {
+            get
+            {
+                return (mixerLine.fdwLine & MixerInterop.MIXERLINE_LINEF.MIXERLINE_LINEF_ACTIVE) != 0;
+            }
+        }
+
+        /// <summary>
+        /// Is this destination disconnected
+        /// </summary>
+        public bool IsDisconnected
+        {
+            get
+            {
+                return (mixerLine.fdwLine & MixerInterop.MIXERLINE_LINEF.MIXERLINE_LINEF_DISCONNECTED) != 0;
+            }
+        }
+
+        /// <summary>
+        /// Is this destination a source
+        /// </summary>
+        public bool IsSource
+        {
+            get
+            {
+                return (mixerLine.fdwLine & MixerInterop.MIXERLINE_LINEF.MIXERLINE_LINEF_SOURCE) != 0;
+            }
+        }
+
+
+		/// <summary>
+		/// Gets the specified source
+		/// </summary>
+		public MixerLine GetSource(int sourceIndex) 
+		{
+			if(sourceIndex < 0 || sourceIndex >= SourceCount) 
+			{
+				throw new ArgumentOutOfRangeException("nSource");
+			}
+            return new MixerLine(mixerHandle, destinationIndex, sourceIndex, this.mixerHandleType);			
+		}
+
+        /**
+		/// <summary>
+		/// Gets the specified control
+		/// </summary>
+		public MixerControl GetControl(int controlIndex) 
+		{
+			if(controlIndex < 0 || controlIndex >= ControlsCount) 
+			{
+                throw new ArgumentOutOfRangeException("controlIndex");
+			}
+            return MixerControl.GetMixerControl(mixerHandle, mixerLine.dwLineID, controlIndex+1, Channels, mixerHandleType);
+		}**/
+
+        /// <summary>
+        /// Enumerator for the controls on this Mixer Limne
+        /// </summary>
+        public IEnumerable<MixerControl> Controls
+        {
+            get
+            {
+                /*
+                for (int control = 0; control < ControlsCount; control++)
+                {
+                    yield return GetControl(control);
+                }*/
+                return MixerControl.GetMixerControls(this.mixerHandle, this, this.mixerHandleType);
+            }
+        }
+
+        /// <summary>
+        /// Enumerator for the sources on this Mixer Line
+        /// </summary>
+        public IEnumerable<MixerLine> Sources
+        {
+            get
+            {
+                for (int source = 0; source < SourceCount; source++)
+                {
+                    yield return GetSource(source);
+                }
+            }
+        }
+
+        /// <summary>
+        /// The name of the target output device
+        /// </summary>
+        public string TargetName
+        {
+            get
+            {
+                return mixerLine.szPname;
+            }
+        }
+
+        /// <summary>
+        /// Describes this Mixer Line (for diagnostic purposes)
+        /// </summary>
+        public override string ToString()
+        {
+            return String.Format("{0} {1} ({2} controls, ID={3})", 
+                Name, TypeDescription, ControlsCount, mixerLine.dwLineID);
+        }
+	}
+}
+
---
+++ trunk/csharp/audio/NAudio/Wave/WaveOutputs/WaveOut.cs
@@ -0,0 +1,313 @@
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace NAudio.Wave 
+{
+    /// <summary>
+    /// Represents a wave out device
+    /// </summary>
+    public class WaveOut : IWavePlayer
+    {
+        private IntPtr hWaveOut;
+        private WaveOutBuffer[] buffers;
+        private IWaveProvider waveStream;
+        private int numBuffers;
+        private PlaybackState playbackState;
+        private WaveInterop.WaveOutCallback callback;
+        private int devNumber;
+        private int desiredLatency;
+        private float volume = 1;
+        private WaveOutWindow waveOutWindow;
+        private object waveOutLock;
+
+        /// <summary>
+        /// Indicates playback has stopped automatically
+        /// </summary>
+        public event EventHandler PlaybackStopped;
+
+        /// <summary>
+        /// Retrieves the capabilities of a waveOut device
+        /// </summary>
+        /// <param name="devNumber">Device to test</param>
+        /// <returns>The WaveOut device capabilities</returns>
+        public static WaveOutCapabilities GetCapabilities(int devNumber)
+        {
+            WaveOutCapabilities caps = new WaveOutCapabilities();
+            int structSize = Marshal.SizeOf(caps);
+            MmException.Try(WaveInterop.waveOutGetDevCaps(devNumber, out caps, structSize), "waveOutGetDevCaps");
+            return caps;
+        }
+
+        /// <summary>
+        /// Returns the number of Wave Out devices available in the system
+        /// </summary>
+        public static Int32 DeviceCount
+        {
+            get
+            {
+                return WaveInterop.waveOutGetNumDevs();
+            }
+        }
+
+        /// <summary>
+        /// Opens a WaveOut device
+        /// </summary>
+        /// <param name="devNumber">This is the device number to open. 
+        /// This must be between 0 and <see>DeviceCount</see> - 1.</param>
+        /// <param name="desiredLatency">The number of milliseconds of audio to read before 
+        /// streaming to the audio device. This will be broken into 3 buffers</param>
+        /// <param name="parentWindow">If this parameter is non-null, the Wave Out Messages
+        /// will be sent to the message loop of the supplied control. This is considered a
+        /// safer way to use the waveOut functionality. If this parameter is null, we use a
+        /// lock to ensure that no two threads can call WaveOut functions at the same time, which
+        /// can cause lockups or crashes with some drivers</param>
+        [Obsolete]
+        public WaveOut(int devNumber, int desiredLatency, System.Windows.Forms.Control parentWindow)
+            : this(devNumber, desiredLatency, parentWindow != null)
+        {
+
+        }
+
+        /// <summary>
+        /// Opens a WaveOut device
+        /// </summary>
+        /// <param name="devNumber">This is the device number to open. 
+        /// This must be between 0 and <see>DeviceCount</see> - 1.</param>
+        /// <param name="desiredLatency">The number of milliseconds of audio to read before 
+        /// streaming to the audio device. This will be broken into 3 buffers</param>
+        /// <param name="windowCallback">If this parameter is true, the Wave Out Messages
+        /// will be sent to the message loop of a Windows form. This is considered a
+        /// safer way to use the waveOut functionality. If this parameter is false, we use a
+        /// lock to ensure that no two threads can call WaveOut functions at the same time, which
+        /// can cause lockups or crashes with some drivers</param>
+        public WaveOut(int devNumber, int desiredLatency, bool windowCallback)
+        {
+            this.devNumber = devNumber;
+            this.desiredLatency = desiredLatency;
+            this.callback = new WaveInterop.WaveOutCallback(Callback);
+            this.waveOutLock = new object();
+            if (windowCallback)
+            {
+                waveOutWindow = new WaveOutWindow(callback);
+                //waveOutWindow.AssignHandle(windowHandle);
+            }
+        }
+
+        /// <summary>
+        /// Initialises the WaveOut device
+        /// </summary>
+        /// <param name="waveProvider">WaveProvider to play</param>
+        public void Init(IWaveProvider waveProvider)
+        {
+            this.waveStream = waveProvider;
+            int bufferSize = waveProvider.WaveFormat.ConvertLatencyToByteSize((desiredLatency + 2) / 3); //waveStream.GetReadSize((desiredLatency + 2) / 3);
+            this.numBuffers = 3;
+
+            MmResult result;
+            lock (waveOutLock)
+            {
+                if (waveOutWindow == null)
+                {
+                    result = WaveInterop.waveOutOpen(out hWaveOut, devNumber, waveStream.WaveFormat, callback, 0, WaveInterop.CallbackFunction);
+                }
+                else
+                {
+                    result = WaveInterop.waveOutOpenWindow(out hWaveOut, devNumber, waveStream.WaveFormat, waveOutWindow.Handle, 0, WaveInterop.CallbackWindow);
+                }
+            }
+            MmException.Try(result,"waveOutOpen");
+
+            buffers = new WaveOutBuffer[numBuffers];
+            playbackState = PlaybackState.Stopped;
+            for (int n = 0; n < numBuffers; n++)
+            {
+                buffers[n] = new WaveOutBuffer(hWaveOut, bufferSize, waveStream, waveOutLock);
+            }
+        }
+
+        /// <summary>
+        /// Start playing the audio from the WaveStream
+        /// </summary>
+        public void Play()
+        {
+            if (playbackState != PlaybackState.Playing)
+            {                
+                if (playbackState == PlaybackState.Stopped)
+                {
+                    for (int n = 0; n < numBuffers; n++)
+                    {
+                        System.Diagnostics.Debug.Assert(buffers[n].InQueue == false, "Adding a buffer that was already queued on play");
+                        if (!buffers[n].OnDone())
+                        {
+                            playbackState = PlaybackState.Stopped;
+                        }
+                    }                    
+                }
+                Resume();
+                playbackState = PlaybackState.Playing;
+
+            }
+        }
+
+        /// <summary>
+        /// Pause the audio
+        /// </summary>
+        public void Pause()
+        {
+            MmResult result;
+            lock (waveOutLock)
+            {
+                result = WaveInterop.waveOutPause(hWaveOut);
+            }
+            if (result != MmResult.NoError)
+                throw new MmException(result, "waveOutPause");
+            playbackState = PlaybackState.Paused;
+        }
+
+        /// <summary>
+        /// Resume playing after a pause from the same position
+        /// </summary>
+        public void Resume()
+        {
+            MmResult result;
+            lock (waveOutLock)
+            {
+                result = WaveInterop.waveOutRestart(hWaveOut);
+            }
+            if (result != MmResult.NoError)
+                throw new MmException(result, "waveOutRestart");
+            playbackState = PlaybackState.Playing;
+        }
+
+        /// <summary>
+        /// Stop and reset the WaveOut device
+        /// </summary>
+        public void Stop()
+        {
+            playbackState = PlaybackState.Stopped;
+            MmResult result;
+            lock (waveOutLock)
+            {
+                result = WaveInterop.waveOutReset(hWaveOut);
+            }
+            if (result != MmResult.NoError)
+                throw new MmException(result, "waveOutReset");
+        }
+
+        /// <summary>
+        /// Playback State
+        /// </summary>
+        public PlaybackState PlaybackState
+        {
+            get { return playbackState; }
+        }
+
+        /// <summary>
+        /// Volume for this device 1.0 is full scale
+        /// </summary>
+        public float Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                volume = value;
+                float left = volume;
+                float right = volume;
+
+                int stereoVolume = (int)(left * 0xFFFF) + ((int)(right * 0xFFFF) << 16);
+                MmResult result;
+                lock (waveOutLock)
+                {
+                    result = WaveInterop.waveOutSetVolume(hWaveOut, stereoVolume);
+                }
+                MmException.Try(result,"waveOutSetVolume");
+            }
+
+        }
+
+
+        #region Dispose Pattern
+
+        /// <summary>
+        /// Closes this WaveOut device
+        /// </summary>
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+            Dispose(true);
+        }
+
+        /// <summary>
+        /// Closes the WaveOut device and disposes of buffers
+        /// </summary>
+        /// <param name="disposing">True if called from <see>Dispose</see></param>
+        protected void Dispose(bool disposing)
+        {
+            Stop();
+            lock (waveOutLock)
+            {
+                WaveInterop.waveOutClose(hWaveOut);
+            }
+            if (disposing)
+            {
+                if (buffers != null)
+                {
+                    for (int n = 0; n < numBuffers; n++)
+                    {
+                        buffers[n].Dispose();
+                    }
+                    buffers = null;
+                }
+                if (waveOutWindow != null)
+                {
+                    //waveOutWindow.ReleaseHandle();
+                    waveOutWindow = null;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Finalizer. Only called when user forgets to call <see>Dispose</see>
+        /// </summary>
+        ~WaveOut()
+        {
+            System.Diagnostics.Debug.Assert(false, "WaveOut device was not closed");
+            Dispose(false);
+        }
+
+        #endregion
+
+        // made non-static so that playing can be stopped here
+        private void Callback(IntPtr hWaveOut, WaveInterop.WaveOutMessage uMsg, Int32 dwUser, WaveHeader wavhdr, int dwReserved)
+        {
+            if (uMsg == WaveInterop.WaveOutMessage.Done)
+            {
+                GCHandle hBuffer = (GCHandle)wavhdr.userData;
+                WaveOutBuffer buffer = (WaveOutBuffer)hBuffer.Target;
+                // check that we're not here through pressing stop
+                if (PlaybackState == PlaybackState.Playing)
+                {
+                    if(!buffer.OnDone())
+                    {
+                        playbackState = PlaybackState.Stopped;
+                        RaisePlaybackStoppedEvent();
+                    }
+                }
+
+                // n.b. this was wrapped in an exception handler, but bug should be fixed now
+            }
+        }
+
+        private void RaisePlaybackStoppedEvent()
+        {
+            if (PlaybackStopped != null)
+            {
+                PlaybackStopped(this, EventArgs.Empty);
+            }
+        }
+    }
+}
