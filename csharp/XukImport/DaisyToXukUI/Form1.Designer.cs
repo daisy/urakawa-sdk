e4f3fc549f93574a6f8a8bb4c0b09d13d256e3a5
rachana-singh <rachana-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-04-10 08:59:49 +0000

    Adding the feature to save the xuk files in location choosen by user.

--- csharp/XukImport/DaisyToXukUI/Form1.Designer.cs
+++ csharp/XukImport/DaisyToXukUI/Form1.Designer.cs
@@ -29,7 +29,10 @@
         private void InitializeComponent()
         {
             this.txtBookName = new System.Windows.Forms.TextBox();
+            this.txtSaveBookName = new System.Windows.Forms.TextBox();
             this.btnLoad = new System.Windows.Forms.Button();
+            this.btnStartImport = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
             this.btnClear = new System.Windows.Forms.Button();
             this.btnExit = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
@@ -43,23 +46,57 @@
             this.txtBookName.TabIndex = 0;
             this.txtBookName.Text = "Upload DTBoook";
             // 
+            // txtSaveBookName
+            // 
+            this.txtSaveBookName.Location = new System.Drawing.Point(117, 100);
+            this.txtSaveBookName.Name = "txtSaveBookName";
+            this.txtSaveBookName.Size = new System.Drawing.Size(388, 20);
+            this.txtSaveBookName.TabIndex = 0;
+            this.txtSaveBookName.Text = "Select the path to save DTBoook";
+            // 
             // btnLoad
             // 
             this.btnLoad.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnLoad.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnLoad.Location = new System.Drawing.Point(22, 72);
+            this.btnLoad.Location = new System.Drawing.Point(10, 72);
             this.btnLoad.Name = "btnLoad";
-            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.Size = new System.Drawing.Size(105, 25);
             this.btnLoad.TabIndex = 1;
-            this.btnLoad.Text = "&Load";
+            this.btnLoad.Text = "Select Im&port";
             this.btnLoad.UseVisualStyleBackColor = true;
-            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click_1);
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnStartImport
+            // 
+            this.btnStartImport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnStartImport.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartImport.Location = new System.Drawing.Point(120, 130);
+            this.btnStartImport.Name = "btnStartImport";
+            this.btnStartImport.Size = new System.Drawing.Size(95, 25);
+            this.btnStartImport.TabIndex = 1;
+            this.btnStartImport.Enabled = false;
+            this.btnStartImport.Text = "Start &Import";
+            this.btnStartImport.UseVisualStyleBackColor = true;
+            this.btnStartImport.Click += new System.EventHandler(this.btnStartImport_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSave.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSave.Location = new System.Drawing.Point(10, 100);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(105, 25);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "&Save";
+            this.btnSave.Enabled = false;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnClear
             // 
             this.btnClear.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnClear.Location = new System.Drawing.Point(160, 124);
+            this.btnClear.Location = new System.Drawing.Point(270, 130);
             this.btnClear.Name = "btnClear";
             this.btnClear.Size = new System.Drawing.Size(75, 23);
             this.btnClear.TabIndex = 2;
@@ -71,7 +108,7 @@
             // 
             this.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(357, 124);
+            this.btnExit.Location = new System.Drawing.Point(407,130);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(75, 23);
             this.btnExit.TabIndex = 3;
@@ -98,7 +135,10 @@
             this.Controls.Add(this.btnExit);
             this.Controls.Add(this.btnClear);
             this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnStartImport);
+            this.Controls.Add(this.btnSave);
             this.Controls.Add(this.txtBookName);
+            this.Controls.Add(this.txtSaveBookName);
             this.Name = "Form1";
             this.Text = "Converting DTBook To Xuk Format";
             this.ResumeLayout(false);
@@ -109,7 +149,10 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtBookName;
+        private System.Windows.Forms.TextBox txtSaveBookName;
+        private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnStartImport;
         private System.Windows.Forms.Button btnClear;
         private System.Windows.Forms.Button btnExit;
         private System.Windows.Forms.Label label1;
