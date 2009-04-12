6ec7b2062814a79205da16d33018e341c496b98d
Daniel Weck <625201+danielweck@users.noreply.github.com>
2009-04-12 00:43:18 +0000

    added another port of JLayer

--- csharp/audio/AudioLib/WavFormatConverter.cs
+++ csharp/audio/AudioLib/WavFormatConverter.cs
@@ -134,6 +134,12 @@ namespace AudioLib
         /// <exception cref="NotImplementedException">NOT IMPLEMENTED !</exception>
         public string UnCompressMp3File(string sourceFile, string destinationDirectory, PCMFormatInfo destinationPCMFormat)
         {
+            if (!File.Exists(sourceFile))
+                throw new FileNotFoundException("Invalid source file path");
+
+            if (!Directory.Exists(destinationDirectory))
+                throw new FileNotFoundException("Invalid destination directory");
+
             string destinationFilePath = null;
             PCMFormatInfo pcmFormat = null;
             using (Mp3FileReader mp3Reader = new Mp3FileReader(sourceFile))
@@ -144,10 +150,10 @@ namespace AudioLib
                                                             (uint)pcmStream.WaveFormat.SampleRate,
                                                             (ushort)pcmStream.WaveFormat.BitsPerSample);
 
-                    destinationFilePath = GenerateOutputFileFullname(sourceFile+".wav", destinationDirectory, pcmFormat);
+                    destinationFilePath = GenerateOutputFileFullname(sourceFile + ".wav", destinationDirectory, pcmFormat);
                     using (WaveFileWriter writer = new WaveFileWriter(destinationFilePath, pcmStream.WaveFormat))
                     {
-                        const int BUFFER_SIZE = 1024 * 300; // 300 KB MAX BUFFER  
+                        const int BUFFER_SIZE = 1024 * 8; // 8 KB MAX BUFFER  
                         byte[] buffer = new byte[BUFFER_SIZE];
                         int byteRead;
                         try
@@ -160,11 +166,59 @@ namespace AudioLib
                         }
                         catch (Exception ex)
                         {
-                            return null;
+                            pcmStream.Close();
+                            writer.Close();
+                            pcmFormat = null;
                         }
                     }
                 }
             }
+
+            if (pcmFormat == null)
+            {
+                Stream fileStream = File.Open(sourceFile, FileMode.Open, FileAccess.Read);
+                if (fileStream != null)
+                {
+                    int totalBytesWritten = 0;
+                    using (NLayerMp3Stream mp3Stream = new NLayerMp3Stream(fileStream))
+                    {
+                        pcmFormat = new PCMFormatInfo((ushort)mp3Stream.WaveFormat.Channels,
+                                                      (uint)mp3Stream.WaveFormat.SampleRate,
+                                                      (ushort)mp3Stream.WaveFormat.BitsPerSample);
+                        destinationFilePath = GenerateOutputFileFullname(sourceFile + ".wav", destinationDirectory, pcmFormat);
+                        using (WaveFileWriter writer = new WaveFileWriter(destinationFilePath, mp3Stream.WaveFormat))
+                        {
+                            int buffSize = mp3Stream.GetReadSize(4000);
+                            //const int BUFFER_SIZE = 1024 * 8; // 8 KB MAX BUFFER  
+                            byte[] buffer = new byte[buffSize];
+                            try
+                            {
+                                int byteRead;
+                                writer.Flush();
+                                while ((byteRead = mp3Stream.Read(buffer, 0, buffer.Length)) > 0)
+                                {
+                                    writer.WriteData(buffer, 0, byteRead);
+                                    totalBytesWritten += byteRead;
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                writer.Close();
+                                return null;
+                            }
+                        }
+                    }
+                    if (totalBytesWritten == 0)
+                    {
+                        return null;
+                    }
+                }
+                else
+                {
+                    return null;
+                }
+            }
+
             if (!pcmFormat.IsCompatibleWith(destinationPCMFormat))
             {
                 return ConvertSampleRate(destinationFilePath, destinationDirectory, destinationPCMFormat);
