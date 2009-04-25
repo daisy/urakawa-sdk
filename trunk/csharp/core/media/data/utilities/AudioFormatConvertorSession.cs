79b0c139e68a833085de90c2e9fe789335ecb37e
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-04-25 11:13:37 +0000

    adding class for audio format conversion session. Have basic functionality, will be expanded.

---
+++ trunk/csharp/core/media/data/utilities/AudioFormatConvertorSession.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace urakawa.media.data.utilities
+    {
+    /// <summary>
+    ///  class to maintain session for importing files of format different from default format of project
+    /// will reduce re conversion of files already converted in the same session
+    /// </summary>
+    public class AudioFormatConvertorSession
+        {
+        private Dictionary<string, string> m_FilePathsMap; // maps source files to converted files
+        Presentation m_Presentation;
+
+
+                public AudioFormatConvertorSession ( Presentation presentation)
+            {
+            m_FilePathsMap = new Dictionary<string, string> ();
+            m_Presentation = presentation;
+                                                    }
+
+/// <summary>
+/// checks if wav file or mp3 file has already been converted in the same session, if it is already converted, returns file path of converted file
+/// else convert to wav file with default project format and stores it in data directory of project.
+/// </summary>
+/// <param name="sourceFilePath"></param>
+/// <returns> full path of converted file if conversion is successful </returns>
+        public string ConvertAudioFileFormat ( string sourceFilePath )
+            {
+            if ( !File.Exists( sourceFilePath ))
+                throw new System.IO.FileNotFoundException () ;
+
+            if (m_FilePathsMap.ContainsKey ( sourceFilePath ))
+                {
+                return m_FilePathsMap[sourceFilePath];
+                }
+            else
+                {
+                                string convertedFilePath =  AudioFormatConvertor.ConvertToDefaultFormat ( m_Presentation, sourceFilePath );
+
+                                if (File.Exists ( convertedFilePath ))
+                                    {
+                                    m_FilePathsMap.Add ( sourceFilePath, convertedFilePath );
+                                    return convertedFilePath;
+                                    }
+                                else
+                                    {
+                                    throw new System.Exception ( "Operation failed" );
+                                    }
+                }
+
+}
+
+
+        }
+    }
