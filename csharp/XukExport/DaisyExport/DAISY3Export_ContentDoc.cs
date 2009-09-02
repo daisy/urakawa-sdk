b1ec75a0c53926db1ac5c49f6ce3da63a9c5a229
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2009-09-02 18:56:36 +0000

    some more update to export. far from complete yet.

---
+++ csharp/XukExport/DaisyExport/DAISY3Export_ContentDoc.cs
@@ -0,0 +1,131 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml;
+
+
+namespace DaisyExport
+    {
+    public partial class DAISY3Export
+        {
+        // DAISY3Export_ContentDoc
+
+        private void CreateDTBookDocument ()
+            {
+            XmlDocument DTBookDocument = CreateStub_DTBDocument ();
+
+            // add metadata
+            XmlNode headNode = DTBookDocument.GetElementsByTagName ( "head" )[0];
+
+            List<urakawa.metadata.Metadata> metadataList = m_Presentation.Metadatas.ContentsAs_ListCopy;
+
+            foreach (urakawa.metadata.Metadata m in metadataList)
+                {
+                XmlNode metaNode = DTBookDocument.CreateElement ( null, "meta", headNode.NamespaceURI );
+                if (m.NameContentAttribute != null)
+                    {
+                    headNode.AppendChild ( metaNode );
+                    CommonFunctions.CreateAppendXmlAttribute ( DTBookDocument, metaNode, "name", m.NameContentAttribute.Name );
+                    CommonFunctions.CreateAppendXmlAttribute ( DTBookDocument, metaNode, "content", m.NameContentAttribute.Value );
+                    }
+                }
+
+            // add elements to book body
+            Dictionary<urakawa.core.TreeNode, XmlNode> treeNode_XmlNodeMap = new Dictionary<urakawa.core.TreeNode, XmlNode> ();
+
+            urakawa.core.TreeNode rNode = m_Presentation.RootNode;
+            XmlNode bookNode = DTBookDocument.GetElementsByTagName ( "book" )[0];
+
+
+            treeNode_XmlNodeMap.Add ( rNode, bookNode );
+            XmlNode currentXmlNode = null;
+
+            rNode.AcceptDepthFirst (
+                    delegate ( urakawa.core.TreeNode n )
+                        {
+
+                        urakawa.property.xml.XmlProperty xmlProp = n.GetProperty<urakawa.property.xml.XmlProperty> ();
+                        if (xmlProp != null && xmlProp.LocalName != "book")
+                            {
+                            // create sml node in dtbook document
+
+                            string name = xmlProp.LocalName;
+                            string prefix = name.Contains ( ":" ) ? name.Split ( ':' )[0] : null;
+                            string elementName = name.Contains ( ":" ) ? name.Split ( ':' )[1] : name;
+                            currentXmlNode = DTBookDocument.CreateElement ( prefix, elementName, bookNode.NamespaceURI );
+
+                            // add attributes
+                            if (xmlProp.Attributes != null && xmlProp.Attributes.Count > 0)
+                                {
+                                for (int i = 0; i < xmlProp.Attributes.Count; i++)
+                                    {
+                                    CommonFunctions.CreateAppendXmlAttribute ( DTBookDocument,
+                                        currentXmlNode,
+                                        xmlProp.Attributes[i].LocalName,
+    xmlProp.Attributes[i].Value );
+                                    }
+                                } // attribute nodes created
+
+                            // add text from text property
+
+                            string txt = n.GetTextMedia () != null ? n.GetTextMedia ().Text : null;
+                            if (txt != null)
+                                {
+                                XmlNode textNode = DTBookDocument.CreateTextNode ( txt );
+                                currentXmlNode.AppendChild ( textNode );
+                                }
+
+                            // add current node to its parent
+                            treeNode_XmlNodeMap[n.Parent].AppendChild ( currentXmlNode );
+
+                            // add nodes to dictionary 
+                            treeNode_XmlNodeMap.Add ( n, currentXmlNode );
+                            return true;
+                            }
+
+                        return true;
+                        },
+                    delegate ( urakawa.core.TreeNode n ) { } );
+
+
+
+            CommonFunctions.WriteXmlDocumentToFile ( DTBookDocument,
+                Path.Combine ( System.AppDomain.CurrentDomain.BaseDirectory, "try.xml" ) );
+
+            }
+
+
+        private XmlDocument CreateStub_DTBDocument ()
+            {
+            XmlDocument DTBDocument = new XmlDocument ();
+            DTBDocument.XmlResolver = null;
+
+            DTBDocument.CreateXmlDeclaration ( "1.0", "utf-8", null );
+            DTBDocument.AppendChild ( DTBDocument.CreateDocumentType ( "dtbook",
+                "-//NISO//DTD dtbook 2005-1//EN",
+                "http://www.daisy.org/z3986/2005/dtbook-2005-1.dtd",
+                null ) );
+
+            XmlNode DTBNode = DTBDocument.CreateElement ( null,
+                "dtbook",
+                "http://www.daisy.org/z3986/2005/dtbook/" );
+
+            DTBDocument.AppendChild ( DTBNode );
+
+
+            CommonFunctions.CreateAppendXmlAttribute ( DTBDocument, DTBNode, "version", "2005-1" );
+            CommonFunctions.CreateAppendXmlAttribute ( DTBDocument, DTBNode, "xml:lang", "en" );
+
+
+            XmlNode headNode = DTBDocument.CreateElement ( null, "head", DTBNode.NamespaceURI );
+            DTBNode.AppendChild ( headNode );
+            XmlNode bookNode = DTBDocument.CreateElement ( null, "book", DTBNode.NamespaceURI );
+            DTBNode.AppendChild ( bookNode );
+
+            return DTBDocument;
+            }
+
+
+        }
+    }
