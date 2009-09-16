7f31c55c199dbad91fa9385edf6da4ab6f762f7f
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-09-16 18:35:49 +0000

    other attributes also written with metadata.

--- trunk/csharp/XukExport/DaisyExport/DAISY3Export_Opf.cs
+++ trunk/csharp/XukExport/DaisyExport/DAISY3Export_Opf.cs
@@ -20,7 +20,7 @@ namespace DaisyExport
             const string mediaType_Opf = "text/xml";
             const string mediaType_Ncx = "application/x-dtbncx+xml";
             const string mediaType_Image_Jpg = "image/jpeg";
-            const string mediaType_Image_Png =  "image/png" ;
+            const string mediaType_Image_Png = "image/png";
 
             XmlNode itemNode = opfDocument.CreateElement ( null, "item", manifestNode.NamespaceURI );
             manifestNode.AppendChild ( itemNode );
@@ -75,9 +75,9 @@ namespace DaisyExport
                 string strID = GetNextID ( ID_OpfPrefix );
                 CommonFunctions.CreateAppendXmlAttribute ( opfDocument, itemNode, "id", strID );
 
-                if (string.Compare ( Path.GetExtension ( imageFileName ), ".png", true ) == 0 )
+                if (string.Compare ( Path.GetExtension ( imageFileName ), ".png", true ) == 0)
                     {
-                                        CommonFunctions.CreateAppendXmlAttribute ( opfDocument, itemNode, "media-type", mediaType_Image_Png );
+                    CommonFunctions.CreateAppendXmlAttribute ( opfDocument, itemNode, "media-type", mediaType_Image_Png );
                     }
                 else
                     {
@@ -109,42 +109,59 @@ namespace DaisyExport
 
             foreach (urakawa.metadata.Metadata m in m_Presentation.Metadatas.ContentsAs_ListCopy)
                 {
+                XmlNode metadataNodeCreated = null;
                 if (m.NameContentAttribute.Name.StartsWith ( "dc:" ))
                     {
                     if (m.NameContentAttribute.Name == "dc:format")
                         {
-                        AddMetadataAsInnerText ( opfDocument, dc_metadataNode, m.NameContentAttribute.Name, "ANSI/NISO Z39.86-2005" );
+                        metadataNodeCreated = AddMetadataAsInnerText ( opfDocument, dc_metadataNode, m.NameContentAttribute.Name, "ANSI/NISO Z39.86-2005" );
                         }
                     else
                         {
-                        AddMetadataAsInnerText ( opfDocument, dc_metadataNode, m.NameContentAttribute.Name, m.NameContentAttribute.Value );
+                        metadataNodeCreated = AddMetadataAsInnerText ( opfDocument, dc_metadataNode, m.NameContentAttribute.Name, m.NameContentAttribute.Value );
                         }
                     }
                 else
                     {
-                    AddMetadataAsAttributes ( opfDocument, x_metadataNode, m.NameContentAttribute.Name, m.NameContentAttribute.Value );
+                    metadataNodeCreated = AddMetadataAsAttributes ( opfDocument, x_metadataNode, m.NameContentAttribute.Name, m.NameContentAttribute.Value );
                     }
-                }
+                // add other metadata attributes if any
+                foreach (urakawa.metadata.MetadataAttribute ma in m.OtherAttributes.ContentsAs_ListCopy)
+                    {
+                    CommonFunctions.CreateAppendXmlAttribute ( opfDocument, metadataNodeCreated, ma.Name, ma.Value );
+                    }
+
+                } // end of metadata for each loop
+
 
             // add uid to dc:identifier
-            XmlNode identifierMetaNode = opfDocument.GetElementsByTagName ( "dc:Identifier" )[0];
-            if (identifierMetaNode != null)
+            XmlNodeList identifierList = opfDocument.GetElementsByTagName ( "dc:Identifier" );
+            bool isUidReAssigned = false;
+            foreach (XmlNode identifierMetaNode in identifierList)
                 {
-                CommonFunctions.CreateAppendXmlAttribute ( opfDocument, identifierMetaNode, "id", "uid" );
+                if (identifierMetaNode.Attributes.GetNamedItem ( "uid" ) != null)
+                    {
+                    identifierMetaNode.Attributes.GetNamedItem ( "uid" ).Value = "uid";
+                    isUidReAssigned = true;
+                    }
+                }
+            if (!isUidReAssigned)
+                {
+                CommonFunctions.CreateAppendXmlAttribute ( opfDocument, identifierList[0], "id", "uid" );
                 }
             }
 
-        private void AddMetadataAsInnerText ( XmlDocument doc, XmlNode metadataParentNode, string name, string content )
+        private XmlNode AddMetadataAsInnerText ( XmlDocument doc, XmlNode metadataParentNode, string name, string content )
             {
             XmlNode node = null;
-            
+
             if (name.Contains ( ":" ))
                 {
                 // split the metadata name and make first alphabet upper, required for daisy 3.0
-                string  splittedName = name.Split ( ':' )[1] ;
-                splittedName  = splittedName.Substring (0,1).ToUpper () + splittedName.Remove (0,1 ) ;
+                string splittedName = name.Split ( ':' )[1];
+                splittedName = splittedName.Substring ( 0, 1 ).ToUpper () + splittedName.Remove ( 0, 1 );
 
-                node = doc.CreateElement ( name.Split ( ':' )[0], splittedName,metadataParentNode.Attributes.GetNamedItem ("xmlns:dc").Value );
+                node = doc.CreateElement ( name.Split ( ':' )[0], splittedName, metadataParentNode.Attributes.GetNamedItem ( "xmlns:dc" ).Value );
                 }
             else
                 {
@@ -154,6 +171,7 @@ namespace DaisyExport
             node.AppendChild (
                 doc.CreateTextNode ( content ) );
 
+            return node;
             }
 
         private XmlDocument CreateStub_OpfDocument ()
