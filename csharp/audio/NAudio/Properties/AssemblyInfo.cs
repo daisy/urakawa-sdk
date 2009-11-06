89b480b6798d148a025273daf8c6288d31c62c63
Daniel Weck <625201+danielweck@users.noreply.github.com>
2009-11-06 17:32:46 +0000

    some required project configuration changes to support the .NET 3.5 Client Profile inside the Tobi solution. This is likely to break the Visual Studio 2005 build, so we need to create a separate project file for unit-tests and other out-of-Tobi Urakawa SDK developments

--- csharp/audio/NAudio/Properties/AssemblyInfo.cs
+++ csharp/audio/NAudio/Properties/AssemblyInfo.cs
@@ -14,15 +14,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-[assembly: InternalsVisibleTo("NAudioTests")]
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e82fa7f0-f952-4d93-b7b0-392bbf53b2a4")]
-
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
