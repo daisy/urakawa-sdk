fb15a354e3c68412deef97b850aca27fd4d9497c
daniel-weck <daniel-weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-11-06 22:48:05 +0000

    many small changes to make sure the code is compatible with the .NET 3.5 Client Profile

--- trunk/csharp/audio/NAudio/Gui/Arranger.cs
+++ trunk/csharp/audio/NAudio/Gui/Arranger.cs
@@ -1,10 +1,3 @@
-using System;
-using System.Collections;
-using System.ComponentModel;
-using System.Drawing;
-using System.Data;
-using System.Windows.Forms;
-
 namespace NAudio.Gui
 {
 	/// <summary>
