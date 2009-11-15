de494be846fdee57f357cf6de8b04f63f8640054
Daniel Weck <625201+danielweck@users.noreply.github.com>
2009-11-15 08:56:58 +0000

    added missing files

---
+++ csharp/dtds/DTDs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace DTDs
+{
+    public static class DTDs
+    {
+        private static Assembly m_Assembly = Assembly.GetExecutingAssembly();
+        public static Stream Fetch(string resourcePath)
+        {
+            //string[] names = m_Assembly.GetManifestResourceNames();
+
+            return m_Assembly.GetManifestResourceStream(resourcePath);
+        }
+    }
+}
