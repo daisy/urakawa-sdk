f439385c73b7a52c3131608ca1e0176f3dd04583
daniel-weck <daniel-weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-11-18 03:30:29 +0000

    testing .NET 4 failed. now ready to revert to .NET 3.5, if necessary (hopefully the build is not broken though)

--- trunk/csharp/audio/AudioLib/Properties/AssemblyInfo.cs
+++ trunk/csharp/audio/AudioLib/Properties/AssemblyInfo.cs
@@ -27,3 +27,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyFileVersion("2.0.0.0")]
 
 [assembly: ComVisibleAttribute(false)]
+
+#if !NET_3_5 // NET_4_0 || BOOTSTRAP_NET_4_0
+[assembly: System.Security.SecurityRules(System.Security.SecurityRuleSet.Level1)]
+#endif
