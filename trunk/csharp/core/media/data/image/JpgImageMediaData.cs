d84c9765f1caec0a3df6a2490659824828ebbc50
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-12-07 06:50:55 +0000

    adding stub image classes

---
+++ trunk/csharp/core/media/data/image/JpgImageMediaData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace urakawa.media.data.image
+    {
+    public class JpgImageMediaData:ImageMediaData
+        {
+        protected override MediaData CopyProtected ()
+            {
+            throw new NotImplementedException ();
+            }
+
+        protected override MediaData ExportProtected ( Presentation destPres )
+            {
+            throw new NotImplementedException ();
+            }
+
+        public override IEnumerable<DataProvider> UsedDataProviders
+            {
+            get
+                {
+                throw new NotImplementedException ();
+                }
+            }
+
+        public override string GetTypeNameFormatted ()
+            {
+            return xuk.XukStrings.JpgImageMediaData;
+            }
+
+
+
+        }
+    }
