25b56d4b16a9d2ac9e46756148adfcce302506c5
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-12-08 14:18:49 +0000

    adding functions for adding images to respective classes.

--- trunk/csharp/core/media/data/image/BmpImageMediaData.cs
+++ trunk/csharp/core/media/data/image/BmpImageMediaData.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace urakawa.media.data.image
     {
-    public class BmpImageMediaData:ImageMediaData
+    public class BmpImageMediaData : ImageMediaData
         {
+        private DataProvider m_ImageDataProvider;
+        private string m_ImageOriginalName;
+
         protected override MediaData CopyProtected ()
             {
             throw new NotImplementedException ();
@@ -28,6 +32,41 @@ namespace urakawa.media.data.image
         public override string GetTypeNameFormatted ()
             {
             return xuk.XukStrings.BmpImageMediaData;
+            }
+
+
+
+        public override void AddImage ( DataProvider dataProv, string imgOriginalName )
+            {
+            if (dataProv == null)
+                {
+                throw new exception.MethodParameterIsNullException ( "The data provider of a bmp image can not be null" );
+                }
+
+
+            if (dataProv.MimeType != DataProviderFactory.IMAGE_BMP_MIME_TYPE)
+                {
+                throw new exception.OperationNotValidException (
+                    "The mime type of the given DataProvider is not BMP!" );
+                }
+
+            if (string.IsNullOrEmpty ( imgOriginalName ))
+                {
+                throw new exception.MethodParameterIsEmptyStringException ( "original name of image cannot be null!" );
+                }
+
+            m_ImageDataProvider = dataProv;
+            base.ImageOriginalName = imgOriginalName;
+            }
+
+
+
+        protected override DataProvider CreateDataProviderFromNewImage ( string path )
+            {
+            DataProvider imgDataProvider = Presentation.DataProviderFactory.Create ( DataProviderFactory.IMAGE_BMP_MIME_TYPE );
+            ((FileDataProvider)imgDataProvider).InitByCopyingExistingFile ( path );
+
+            return imgDataProvider;
             }
 
 
