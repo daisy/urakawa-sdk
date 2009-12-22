7437a028760da86647465ef033b3327c43a20650
chhavi-negi <chhavi-negi@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-12-22 14:54:04 +0000

    Removed a minor error.

--- csharp/toybox/StoreSettings/WindowsApplication1/Form1.cs
+++ csharp/toybox/StoreSettings/WindowsApplication1/Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
-using StoreSettings.Properties;
+using WindowsApplication1.Properties;
 
 namespace StoreSettings
 {
--- csharp/toybox/StoreSettings/WindowsApplication1/Properties/Settings.Designer.cs
+++ csharp/toybox/StoreSettings/WindowsApplication1/Properties/Settings.Designer.cs
@@ -22,5 +22,29 @@ namespace WindowsApplication1.Properties {
                 return defaultInstance;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("0, 0")]
+        public global::System.Drawing.Point WindowLocation {
+            get {
+                return ((global::System.Drawing.Point)(this["WindowLocation"]));
+            }
+            set {
+                this["WindowLocation"] = value;
+            }
+        }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("264, 236")]
+        public global::System.Drawing.Size WindowSize {
+            get {
+                return ((global::System.Drawing.Size)(this["WindowSize"]));
+            }
+            set {
+                this["WindowSize"] = value;
+            }
+        }
     }
 }
