1c5305001c123deba0f705fbb669e74fd380eca8
rachana-singh <rachana-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-12-23 11:27:12 +0000

    Adding manager class for handling priority.

---
+++ csharp/toybox/SettingsDemo/WindowsApplication1/SettingsManager.cs
@@ -0,0 +1,62 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsApplication1
+    {
+    public class SettingsManager
+        {
+        private NewClass1.Class1 mClass1 ;
+        public NewClass1.Class1 Instance_Class1
+            {
+            get { return mClass1; }
+            set { mClass1 = value; }
+            }
+
+        private NewClass2.Class2 mClass2 ;
+
+        public NewClass2.Class2 Instance_Class2
+            {
+            get { return mClass2; }
+            set { mClass2 = value; }
+            }
+
+
+        private Form m_MainForm;
+
+        public SettingsManager (Form mainForm)
+            {
+            m_MainForm = mainForm;
+            }
+
+        public string UserSettingsText_Class1
+            {
+            get 
+                {
+                if (mClass1 != null)
+                    {
+                    return mClass1.UserSettingText1;
+                    }
+                else
+                    {
+                    // return from the main form.
+                    return null;
+                    }
+                }
+            set
+                {
+                if (mClass1 != null)
+                    {
+                    mClass1.UserSettingText1 = value;
+                    }
+                else
+                    {
+                    // set in main form class
+                    }
+                }
+            }
+
+
+        }
+    }
