81d04fdd3f63004ce5e663aa8641ce17a2dae9bd
chhavi-negi <chhavi-negi@6064ade6-bb1e-4b30-8970-879fdf51070b>
2009-12-24 12:43:25 +0000

    Saved default Settings for main form.

--- csharp/toybox/SettingsDemo/WindowsApplication1/Form1.cs
+++ csharp/toybox/SettingsDemo/WindowsApplication1/Form1.cs
@@ -11,56 +11,59 @@ namespace SettingsDemo
 {
     public partial class Form1 : Form
     {
-        private NewClass1.Class1 mClass1 = new NewClass1.Class1();
-        private NewClass2.Class2 mClass2 = new NewClass2.Class2();
-        private WindowsApplication1.SettingsManager mSetMgr;
+        private NewClass1.Class1 m_Class1 = new NewClass1.Class1();
+        private NewClass2.Class2 m_Class2 = new NewClass2.Class2();
+        
+        private WindowsApplication1.SettingsManager m_SetMgr;
 
         public Form1()
         {
             InitializeComponent();
-            
         }
-      public System.Drawing.Size mSize 
+        public System.Drawing.Size MainFormSize 
       {
           get { return Settings.Default.WindowSize; }
-          set { Settings.Default.WindowSize = value; }
-
+          set {  Settings.Default.WindowSize = value; }
       }
+
         private void Form1_Load(object sender, EventArgs e)
         {
-        mSetMgr = new WindowsApplication1.SettingsManager ( this );
-        mSetMgr.Instance_Class1 = mClass1;
-        mSetMgr.Instance_Class2 = mClass2;
+        m_SetMgr = new WindowsApplication1.SettingsManager ( this );
+        m_SetMgr.Instance_Class1 = m_Class1;
+        m_SetMgr.Instance_Class2 = m_Class2;
+        
             if (Settings.Default.WindowSize != null)
             {
-                this.Size = mSetMgr.FormSize;
+                this.Size = m_SetMgr.FormSize;
             }
-            // Set the text in various text boxes
 
-            textBox1.Text = mClass1.UserSettingText1;    //text from class1 for saving user setting
-            textBox2.Text = mClass2.UserSettingText2;    //text from class2 for saving user setting
-            textBox3.Text = mClass1.AppSettingText;    // text from class1 for saving application setting
+            // Set the text in various text boxes
+           
+            textBox1.Text = m_Class1.UserSettingText1;    //text from class1 for saving user setting
+            textBox2.Text = m_Class2.UserSettingText2;    //text from class2 for saving user setting
+            textBox3.Text = m_Class1.AppSettingText;    // text from class1 for saving application setting
         }
 
              
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
-        {
+        {             
             if (this.WindowState == FormWindowState.Normal)
             {
-                mSetMgr.FormSize = this.Size;
+                m_SetMgr.FormSize = this.Size;
             }
             else
             {
-                mSetMgr.FormSize = this.RestoreBounds.Size;
+                m_SetMgr.FormSize = this.RestoreBounds.Size;
             }
             
             // Copy the text to the settings
-            mClass1.UserSettingText1 = textBox1.Text;        
-            mClass2.UserSettingText2 = textBox2.Text;                     
+            m_Class1.UserSettingText1 = textBox1.Text;        
+            m_Class2.UserSettingText2 = textBox2.Text;                     
            
             // Save the settings
-            mClass1.Save();
-            mClass2.Save();         
+            m_Class1.Save();
+            m_Class2.Save();
+            Settings.Default.Save();  
          }
      }
 }
