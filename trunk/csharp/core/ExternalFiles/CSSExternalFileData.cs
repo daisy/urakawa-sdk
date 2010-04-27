3967b72248961530b0ed69bd4d3a7119c230e1b0
daniel-weck <daniel-weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2010-04-27 12:23:53 +0000

    fixed a major bug in external files: the factory pretty versus compressed names were not refreshed upon switching mode, ans as a result CSS/XSLT/etc files were being deleted during cleanup because the Urakawa SDK backend was not able to create the type via reflection !

--- trunk/csharp/core/ExternalFiles/CSSExternalFileData.cs
+++ trunk/csharp/core/ExternalFiles/CSSExternalFileData.cs
@@ -1,27 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using urakawa.xuk;
+﻿using urakawa.xuk;
 using urakawa.data;
 
 namespace urakawa.ExternalFiles
+{
+    public class CSSExternalFileData : ExternalFileData
     {
-    public class CSSExternalFileData:ExternalFileData
-        {
 
         public override string MimeType
-            {
+        {
             get
-                {
-                return DataProviderFactory.STYLE_CSS_MIME_TYPE;
-                }
-            }
-
-        public override string GetTypeNameFormatted ()
             {
-            return XukStrings.CSSExternalFileData;
+                return DataProviderFactory.STYLE_CSS_MIME_TYPE;
             }
-
         }
+
+        public override string GetTypeNameFormatted()
+        {
+            return XukStrings.CSSExternalFileData;
+        }
+
     }
+}
