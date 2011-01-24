ce995e641374f64956c010b66e61669a7def1f9b
Daniel Weck <625201+danielweck@users.noreply.github.com>
2011-01-24 23:56:00 +0000

    removed obsolete events

--- csharp/core/events/core/PropertyAddedEventArgs.cs
+++ csharp/core/events/core/PropertyAddedEventArgs.cs
@@ -6,25 +6,25 @@ using urakawa.property;
 
 namespace urakawa.events.core
 {
-    /// <summary>
-    /// Arguments of the <see cref="TreeNode.PropertyAdded"/> event
-    /// </summary>
-    public class PropertyAddedEventArgs : TreeNodeEventArgs
-    {
-        /// <summary>
-        /// Constructor setting the source <see cref="TreeNode"/> of the event
-        /// and the <see cref="Property"/> that was added
-        /// </summary>
-        /// <param name="src">The source <see cref="TreeNode"/> of the event</param>
-        /// <param name="addee">The <see cref="Property"/> that was added</param>
-        public PropertyAddedEventArgs(TreeNode src, Property addee) : base(src)
-        {
-            AddedProperty = addee;
-        }
+    ///// <summary>
+    ///// Arguments of the <see cref="TreeNode.PropertyAdded"/> event
+    ///// </summary>
+    //public class PropertyAddedEventArgs : TreeNodeEventArgs
+    //{
+    //    /// <summary>
+    //    /// Constructor setting the source <see cref="TreeNode"/> of the event
+    //    /// and the <see cref="Property"/> that was added
+    //    /// </summary>
+    //    /// <param name="src">The source <see cref="TreeNode"/> of the event</param>
+    //    /// <param name="addee">The <see cref="Property"/> that was added</param>
+    //    public PropertyAddedEventArgs(TreeNode src, Property addee) : base(src)
+    //    {
+    //        AddedProperty = addee;
+    //    }
 
-        /// <summary>
-        /// The <see cref="Property"/> that was added
-        /// </summary>
-        public readonly Property AddedProperty;
-    }
+    //    /// <summary>
+    //    /// The <see cref="Property"/> that was added
+    //    /// </summary>
+    //    public readonly Property AddedProperty;
+    //}
 }
