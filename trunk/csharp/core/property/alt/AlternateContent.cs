b8fc49efef555b40e6e52699463eb87d8dee34b3
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2011-06-06 18:34:27 +0000

    adding stub classes for alternate content

---
+++ trunk/csharp/core/property/alt/AlternateContent.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace urakawa.property.alt
+{
+    public class AlternateContent
+    {
+    }
+}
---
+++ trunk/csharp/core/property/alt/AlternateContentProperty.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using urakawa.xuk;
+
+namespace urakawa.property.alt
+{
+    public class AlternateContentProperty:Property
+    {
+        public override string GetTypeNameFormatted()
+        {
+            return XukStrings.AlternateContentProperty;
+        }
+
+    }
+}
