03d760a52ff73c0ac26fb4f99b40b07daf4bbdc3
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2011-06-10 08:54:35 +0000

    added xukin, xukout for AlternateContentProperty

--- trunk/csharp/core/property/alt/AlternateContentProperty.cs
+++ trunk/csharp/core/property/alt/AlternateContentProperty.cs
@@ -1,9 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Xml;
 
 using urakawa.xuk;
-
+using urakawa.progress;
 namespace urakawa.property.alt
 {
     public class AlternateContentProperty:Property
@@ -35,6 +36,24 @@ namespace urakawa.property.alt
                     
                 return m_AlternateContents; 
             } 
+        }
+
+        protected override void XukInChild(XmlReader source, IProgressHandler handler)
+        {
+            if (source.LocalName == XukStrings.AlternateContents)
+            {
+                AlternateContents.XukIn(source, handler);
+            }
+            else
+            {
+                base.XukInChild(source, handler);
+            }
+        }
+
+        protected override void XukOutChildren(XmlWriter destination, Uri baseUri, IProgressHandler handler)
+        {
+            base.XukOutChildren(destination, baseUri, handler);
+            AlternateContents.XukOut(destination, baseUri, handler);
         }
 
     }
