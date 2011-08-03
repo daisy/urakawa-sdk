238ae33d230b898eb1c0cf206415fe3cfaf46c69
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2011-08-03 08:55:05 +0000

    hooked up description function to enable execution. Yet to incorporate ongoing data model changes.

--- trunk/csharp/daisy/export/Daisy3_Export_ContentDoc.cs
+++ trunk/csharp/daisy/export/Daisy3_Export_ContentDoc.cs
@@ -320,6 +320,19 @@ namespace urakawa.daisy.export
 
                                 imgSrcAttribute.Value = exportImageName;
 
+                                if (n.GetAlternateContentProperty() != null)
+                                {
+                                    string descriptionFile =  CreateImageDescription(exportImageName, n.GetAlternateContentProperty());
+                                    if (!String.IsNullOrEmpty(descriptionFile))
+                                    {
+                                        // short term way for executing image description code: will be updated in later phase of implementation
+                                        XmlNode anchorNode = DTBookDocument.CreateElement("a", currentXmlNode.NamespaceURI);
+                                        currentXmlNode.AppendChild(anchorNode);
+                                        XmlDocumentHelper.CreateAppendXmlAttribute(DTBookDocument, anchorNode, "href", descriptionFile);
+                                        anchorNode.AppendChild(DTBookDocument.CreateTextNode("Image description"));
+                                    }
+                                }
+
                                 if (!m_FilesList_Image.Contains(exportImageName))
                                 {
                                     m_FilesList_Image.Add(exportImageName);
