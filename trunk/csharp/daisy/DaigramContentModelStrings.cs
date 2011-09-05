dd47259b16decf63b4f57fe6ead29a169cebf30c
avneesh-singh <avneesh-singh@6064ade6-bb1e-4b30-8970-879fdf51070b>
2011-09-05 08:56:24 +0000

    adding static class for containing all DIAGRAM content model strings for Import / Export and application

---
+++ trunk/csharp/daisy/DaigramContentModelStrings.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace urakawa.daisy
+{
+    public static class DaigramContentModelStrings
+    {
+        public static string Summary { get { return "d:summary"; } }
+        public static string LondDesc { get { return "d:longDesc"; } }
+        public static string SimplifiedLanguageDescription { get { return "d:simplifiedLanguageDescription "; } }
+        public static string Tactile { get { return "d:tactile"; } }
+        public static string Tour { get { return "d:tour"; } }
+        public static string SimplifiedImage { get { return "d:simplifiedImage "; } }
+        public static string Block { get { return "block"; } }
+        public static string Annotation { get { return "annotation "; } }
+        
+    }
+}
