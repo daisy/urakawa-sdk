f711c53a6d8ce3c14025352d70134d0851281613
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2011-09-05 09:10:50 +0000

    added list for diagram metadata names, yet to append all names

--- csharp/daisy/DaigramContentModelStrings.cs
+++ csharp/daisy/DaigramContentModelStrings.cs
@@ -14,6 +14,27 @@ namespace urakawa.daisy
         public static string SimplifiedImage { get { return "d:simplifiedImage "; } }
         public static string Block { get { return "block"; } }
         public static string Annotation { get { return "annotation "; } }
-        
+
+        private static List<string> m_MetadataNames = null ;
+        public static List<string> MetadataNames
+        {
+            get
+            {
+                if (m_MetadataNames == null)
+                {
+                    InitializeMetadataList();
+                }
+                return m_MetadataNames;
+            }
+        }
+
+        private static void InitializeMetadataList()
+        {
+            m_MetadataNames = new List<string>();
+            m_MetadataNames.Add("z3986:name");
+            m_MetadataNames.Add("z3986:version");
+            m_MetadataNames.Add("dc:identifier");
+        }
+
     }
 }
