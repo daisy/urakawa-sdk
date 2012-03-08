85478e6f40dc9a658e95b0a977135954d6054f23
daniel-weck <daniel-weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2012-03-08 14:34:53 +0000

    SVG and GIF ImageMediaData support

---
+++ trunk/csharp/core/media/data/image/codec/GifImageMediaData.cs
@@ -0,0 +1,19 @@
+﻿using urakawa.data;
+
+namespace urakawa.media.data.image.codec
+{
+    public class GifImageMediaData : ImageMediaData
+    {
+        public override string MimeType
+        {
+            get { return DataProviderFactory.IMAGE_GIF_MIME_TYPE; }
+        }
+
+        public override string GetTypeNameFormatted()
+        {
+            return xuk.XukStrings.GifImageMediaData;
+        }
+    }
+}
+
+
---
+++ trunk/csharp/core/media/data/image/codec/SvgImageMediaData.cs
@@ -0,0 +1,19 @@
+﻿using urakawa.data;
+
+namespace urakawa.media.data.image.codec
+{
+    public class SvgImageMediaData : ImageMediaData
+    {
+        public override string MimeType
+        {
+            get { return DataProviderFactory.IMAGE_SVG_MIME_TYPE; }
+        }
+
+        public override string GetTypeNameFormatted()
+        {
+            return xuk.XukStrings.SvgImageMediaData;
+        }
+    }
+}
+
+
