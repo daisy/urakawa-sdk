fe273f09197b74425c388934b7d0945a451e4871
Avneesh Singh <13777742+avneeshsingh@users.noreply.github.com>
2012-06-07 02:47:03 +0000

    added NCXExternalFileData.cs

---
+++ csharp/core/ExternalFiles/NCXExternalFileData.cs
@@ -0,0 +1,23 @@
+﻿using urakawa.xuk;
+using urakawa.data;
+
+namespace urakawa.ExternalFiles
+{
+    public class NCXExternalFileData : ExternalFileData
+    {
+
+        public override string MimeType
+        {
+            get
+            {
+                return DataProviderFactory.STYLE_NCX_MIME_TYPE;
+            }
+        }
+
+        public override string GetTypeNameFormatted()
+        {
+            return XukStrings.NCXExternalFileData;
+        }
+
+    }
+}
