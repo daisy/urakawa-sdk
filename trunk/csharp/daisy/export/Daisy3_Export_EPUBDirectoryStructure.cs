f8fb1a080ae181922d9269e5276666fdf3ad2009
daniel-weck <daniel-weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2012-07-13 14:25:55 +0000

    removed daisy.export dependency on SharpZip (now using ZipStore single class for compress and deflate of EPUB books)

--- trunk/csharp/daisy/export/Daisy3_Export_EPUBDirectoryStructure.cs
+++ trunk/csharp/daisy/export/Daisy3_Export_EPUBDirectoryStructure.cs
@@ -6,17 +6,21 @@ using System.IO;
 
 using urakawa;
 using urakawa.core;
+
+
+#if ENABLE_SHARPZIP
 using ICSharpCode.SharpZipLib.Zip;
 using ICSharpCode.SharpZipLib.Core;
+#else
+using Jaime.Olivares;
+#endif
 
 namespace urakawa.daisy.export
 {
     public partial class Daisy3_Export
     {
-
         public void CreateEPUBDirectoryStructure()
         {
-
             Directory.CreateDirectory(Path.Combine(m_OutputDirectory, "META-INF"));
             Directory.CreateDirectory(Path.Combine(m_OutputDirectory, "OPS"));
 
@@ -41,79 +45,105 @@ namespace urakawa.daisy.export
                 }
             }
              */
-            PackageToZip();
+
+
+            string parentDirectory = Directory.GetParent(m_OutputDirectory).FullName;
+            string fileName = Path.GetFileName(m_OutputDirectory);
+            string filePath = Path.Combine(parentDirectory, fileName + ".zip");
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+
+            PackageToZip(filePath);
         }
 
-        public void PackageToZip ()
+        public void PackageToZip(string filePath)
         {
+            
+#if ENABLE_SHARPZIP
             FastZipEvents zipEvents = new FastZipEvents();
             zipEvents.ProcessFile = ProcessEvents;
 
             FastZip zip = new FastZip(zipEvents);
-            zip.CreateEmptyDirectories = true ;
+            zip.CreateEmptyDirectories = true;
             //zip.UseZip64 = UseZip64.On;
-            
-            string parentDirectory = Directory.GetParent(m_OutputDirectory).FullName;
-            string fileName = Path.GetFileName(m_OutputDirectory);
-            string filePath = Path.Combine(parentDirectory, fileName + ".zip");
-            if (File.Exists(filePath)) File.Delete(filePath);
 
-            string emptyDirectoryPath = Path.Combine(m_OutputDirectory, fileName);
+            string emptyDirectoryPath = Path.Combine(m_OutputDirectory, Path.GetFileName(m_OutputDirectory));
             Directory.CreateDirectory(emptyDirectoryPath);
             //zip.CreateZip( filePath, m_OutputDirectory, true, null);
             zip.CreateZip(filePath, emptyDirectoryPath, false, null);
-
             Directory.Delete(emptyDirectoryPath);
-            ZipFile zippeFile = new ZipFile(filePath ) ;
+
+            ZipFile zippeFile = new ZipFile(filePath);
             zippeFile.BeginUpdate();
             //zippeFile.Delete(emptyDirectoryPath);
             string initialPath = Directory.GetParent(m_OutputDirectory).FullName;
             //System.Windows.Forms.MessageBox.Show(initialPath);
             //zippeFile.AddDirectory(
-            string mimeTypePath = Path.Combine(m_OutputDirectory,"mimetype")  ;
-            ICSharpCode.SharpZipLib.Zip.StaticDiskDataSource dataSource = new StaticDiskDataSource(mimeTypePath) ;
+            string mimeTypePath = Path.Combine(m_OutputDirectory, "mimetype");
+            ICSharpCode.SharpZipLib.Zip.StaticDiskDataSource dataSource = new StaticDiskDataSource(mimeTypePath);
             //zippeFile.Add(dataSource , Path.Combine(Path.GetFileName (emptyDirectoryPath), "mimetype"), CompressionMethod.Stored);
-            zippeFile.Add(dataSource, mimeTypePath.Replace(initialPath,"") ,CompressionMethod.Stored);
-            
-            string [] listOfFiles = Directory.GetFiles(m_OutputDirectory, "*.*", SearchOption.TopDirectoryOnly);
-            for(int i = 0 ; i < listOfFiles.Length ; i++ )
+            zippeFile.Add(dataSource, mimeTypePath.Replace(initialPath, ""), CompressionMethod.Stored);
+
+            string[] listOfFiles = Directory.GetFiles(m_OutputDirectory, "*.*", SearchOption.TopDirectoryOnly);
+            for (int i = 0; i < listOfFiles.Length; i++)
             {
-                if ( listOfFiles[i] != mimeTypePath )
+                if (listOfFiles[i] != mimeTypePath)
                 {
-                    zippeFile.Add(listOfFiles[i], listOfFiles[i].Replace(initialPath,""));
+                    zippeFile.Add(listOfFiles[i], listOfFiles[i].Replace(initialPath, ""));
                 }
             }
             string[] listOfDirectories = Directory.GetDirectories(m_OutputDirectory, "*.*", SearchOption.AllDirectories);
             for (int i = 0; i < listOfDirectories.Length; i++)
             {
-                
+
                 string[] listOfInternalFiles = Directory.GetFiles(listOfDirectories[i], "*.*", SearchOption.TopDirectoryOnly);
                 //if (listOfInternalFiles.Length == 0) zippeFile.AddDirectory(listOfDirectories[i]);
                 for (int j = 0; j < listOfInternalFiles.Length; j++)
                 {
-                    
-                        zippeFile.Add(listOfInternalFiles[j], listOfInternalFiles[j].Replace(initialPath, ""));
-                    
+                    zippeFile.Add(listOfInternalFiles[j], listOfInternalFiles[j].Replace(initialPath, ""));
                 }
-                
             }
 
             //zippeFile.Add(Path.Combine(m_OutputDirectory, "mimetype"), "mimetype");
-            
+
             zippeFile.CommitUpdate();
 
             zippeFile.Close();
+#else
+            using (ZipStorer zip = ZipStorer.Create(filePath, ""))
+            {
+                zip.EncodeUTF8 = true;
+
+                string mimeTypePath = Path.Combine(m_OutputDirectory, "mimetype");
+                zip.AddFile(ZipStorer.Compression.Store, mimeTypePath, "mimetype", "");
+
+                string basePath = Directory.GetParent(m_OutputDirectory).FullName;
+
+                string[] allFiles = Directory.GetFiles(m_OutputDirectory, "*.*", SearchOption.AllDirectories);
+                for (int i = 0; i < allFiles.Length; i++)
+                {
+                    string fileName = Path.GetFileName(allFiles[i]);
+                    if (allFiles[i] != mimeTypePath
+                        && fileName != ".DS_Store" && fileName != ".svn")
+                    {
+                        zip.AddFile(ZipStorer.Compression.Deflate, allFiles[i], allFiles[i].Replace(basePath, ""), "");
+                    }
+                }
+            }
+#endif
+
+#if DEBUG
             DisplayZipHeaderForVerification(filePath);
+#endif //DEBUG
         }
 
-        private void ProcessEvents(object sender, ScanEventArgs args)
-        {
-        }
-
+#if DEBUG
         private void DisplayZipHeaderForVerification(string zipFilePath)
         {
             BinaryReader reader = new BinaryReader(File.OpenRead(zipFilePath));
-            Console.WriteLine ("1-4. Signatures: " + reader.ReadInt32());
+            Console.WriteLine("1-4. Signatures: " + reader.ReadInt32());
 
             Console.WriteLine("5-6. version: " + reader.ReadInt16());
             Console.WriteLine("7-8. Compression: " + reader.ReadInt16());
@@ -122,6 +152,12 @@ namespace urakawa.daisy.export
             Console.WriteLine("13-16. CRC: " + reader.ReadInt32());
             reader.Close();
         }
+#endif //DEBUG
 
+#if ENABLE_SHARPZIP
+        private void ProcessEvents(object sender, ScanEventArgs args)
+        {
+        }
+#endif //ENABLE_SHARPZIP
     }
 }
