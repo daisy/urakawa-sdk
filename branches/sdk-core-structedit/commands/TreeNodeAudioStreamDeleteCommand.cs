cea515f1973496fdb81ff9981f9f3726f99a5f6c
daniel-weck <daniel-weck@6064ade6-bb1e-4b30-8970-879fdf51070b>
2014-11-28 10:52:09 +0000

    FlowDocument UndoRedo manager architecture upgrade

--- branches/sdk-core-structedit/commands/TreeNodeAudioStreamDeleteCommand.cs
+++ branches/sdk-core-structedit/commands/TreeNodeAudioStreamDeleteCommand.cs
@@ -84,6 +84,8 @@ namespace urakawa.commands
             TreeNode = currentTreeNode;
             SelectionData = selection;
 
+            DebugFix.Assert(SelectionData.m_TreeNode == TreeNode);
+
             ShortDescription = "Delete audio portion";
             LongDescription = "Delete a portion of audio for a given treenode";
 
