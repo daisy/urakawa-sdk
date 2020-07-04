8787d3d1c18cf2b4bd41390b7698731bb5b1017c
rohit_tandon <rohit_tandon@6064ade6-bb1e-4b30-8970-879fdf51070b>
2020-07-04 15:23:36 +0000

    -> commit of file for reading cues.

---
+++ trunk/csharp/audio/AudioLib/ReadCueMarkers.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NAudio.Wave;
+using NAudio.Wave.SampleProviders;
+using NAudio.Wave.WaveFormats;
+using NAudio.Dsp;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace AudioLib
+{
+   public class ReadCueMarkers
+    {
+       private CueWaveFileReader m_CueReader;
+       private List<decimal> m_CuePointsList;
+       private string[] m_CueLabelList;
+
+       public ReadCueMarkers(string pathOfAudioFile)
+       {
+
+           m_CueReader = new CueWaveFileReader(pathOfAudioFile);
+           ReadCues();
+       }
+       private void ReadCues()
+       {
+
+           if (m_CueReader.Cues != null)
+           {
+               //MessageBox.Show(m_CueReader.Cues.Count.ToString());
+               int[]  list = m_CueReader.Cues.CuePositions;
+               m_CueLabelList = m_CueReader.Cues.CueLabels;
+               int cueLabelCount = 0;
+               m_CuePointsList = new List<decimal>();
+
+               foreach (int cuePoint in list)
+               {
+                   decimal tempCuePointInSec = cuePoint / 44100;
+                   m_CuePointsList.Add(tempCuePointInSec);                
+                   cueLabelCount++;
+               }
+           }
+
+       }
+
+       public List<decimal> ListOfCuePoints
+       {
+           get
+           {
+               return m_CuePointsList;
+           }
+       }
+
+       public string[] ListOfCueLabels
+       {
+           get
+           {
+               return m_CueLabelList;
+           }
+       }
+    }
+}
